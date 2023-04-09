935b554e59b37db2001231fc57234807fa324bf4
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-09 21:12:51 +0700

    Slicing Page Flight

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import ListCompany from "../views/company/ListCompany.vue"
 import EditCompany from "../views/company/EditCompany.vue"
 import ListDepartement from "../views/departemen/ListDepartement.vue"
 import EditDepartement from "../views/departemen/EditDepartement.vue"
+import ListFlight from "../views/flight/ListFlight.vue"
+import EditFlight from "../views/flight/EditFlight.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -56,6 +58,16 @@ const router = createRouter({
       path: '/editdepartement',
       name: 'departementedit',
       component: EditDepartement
+    },
+    {
+      path: '/flight',
+      name: 'flight',
+      component: ListFlight
+    },
+    {
+      path: '/editflight',
+      name: 'flightedit',
+      component: EditFlight
     },
   ]
 })
