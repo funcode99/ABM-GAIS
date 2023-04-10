0b6ec870c58ba399275c6a2bad2ae70e9d9f7c9f
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-10 22:00:22 +0700

    Slicing Page Employee List

--- src/components/karyawan/TableKaryawan.vue
+++ src/components/karyawan/TableKaryawan.vue
@@ -6,12 +6,9 @@ import arrowicon from "../../assets/navbar/icon_arrow.svg";
 
 <template>
   <div class="">
-    <table
-      class="table table-zebra table-compact border w-full rounded-lg"
-    >
-
-      <thead class="text-center font-Montserrat text-sm font-bold">
-        <tr class="">
+    <table class="table table-zebra table-compact w-full rounded-lg">
+      <thead class="text-center font-JakartaSans text-sm font-bold">
+        <tr>
           <th class="relative">
             <span class="flex justify-center">No</span>
             <button class="absolute right-0 top-0 bottom-0">
@@ -53,37 +50,13 @@ import arrowicon from "../../assets/navbar/icon_arrow.svg";
       </thead>
 
       <tbody class="bg-[#F5F5F5]">
-
-        <tr>
-          <td
-          >
-            1
-          </td>
-          <td
-            
-          >
-            2382193892
-          </td>
-          <td
-            
-          >
-            John Mane
-          </td>
-          <td
-            
-          >
-            Male
-          </td>
-          <td
-            
-          >
-            Johnm@gmail.com
-          </td>
-          <td
-            
-          >
-            08723236283112
-          </td>
+        <tr class="font-JakartaSans font-normal text-sm">
+          <td>1</td>
+          <td>2382193892</td>
+          <td>John Mane</td>
+          <td>Male</td>
+          <td>Johnm@gmail.com</td>
+          <td>08723236283112</td>
           <td class="flex flex-wrap gap-4 justify-center">
             <button>
               <img :src="editicon" class="w-6 h-6" />
@@ -94,36 +67,13 @@ import arrowicon from "../../assets/navbar/icon_arrow.svg";
           </td>
         </tr>
 
-        <tr>
-          <td
-          >
-            2
-          </td>
-          <td
-            
-          >
-            2349893929
-          </td>
-          <td
-            
-          >
-            Mae X
-          </td>
-          <td
-            
-          >
-            Female
-          </td>
-          <td
-            
-          >
-            maex@gmail.com
-          </td>
-          <td
-            
-          >
-            0872323298929
-          </td>
+        <tr class="font-JakartaSans font-normal text-sm">
+          <td>2</td>
+          <td>2349893929</td>
+          <td>Mae X</td>
+          <td>Female</td>
+          <td>maex@gmail.com</td>
+          <td>0872323298929</td>
           <td class="flex flex-wrap gap-4 justify-center">
             <button>
               <img :src="editicon" class="w-6 h-6" />
@@ -135,37 +85,13 @@ import arrowicon from "../../assets/navbar/icon_arrow.svg";
           </td>
         </tr>
 
-        <tr>
-          <td
-          
-          >
-            3
-          </td>
-          <td
-            
-          >
-            2344784787
-          </td>
-          <td
-            
-          >
-            Nina Max
-          </td>
-          <td
-            
-          >
-            Female
-          </td>
-          <td
-            
-          >
-            ninam@gmail.com
-          </td>
-          <td
-            
-          >
-            0893283728322
-          </td>
+        <tr class="font-JakartaSans font-normal text-sm">
+          <td>3</td>
+          <td>2344784787</td>
+          <td>Nina Max</td>
+          <td>Female</td>
+          <td>ninam@gmail.com</td>
+          <td>0893283728322</td>
           <td class="flex flex-wrap gap-4 justify-center">
             <button>
               <img :src="editicon" class="w-6 h-6" />
@@ -175,29 +101,26 @@ import arrowicon from "../../assets/navbar/icon_arrow.svg";
             </button>
           </td>
         </tr>
-
       </tbody>
-      
     </table>
   </div>
 </template>
 
 <style scoped>
-  th {
-    padding: 2px;
-    text-align: left;
-    position: relative;
-  }
+th {
+  padding: 2px;
+  text-align: left;
+  position: relative;
+}
 
-  tr td {
-    text-align: center;
-    white-space: nowrap;
-  }
+tr td {
+  text-align: center;
+  white-space: nowrap;
+}
 
-  tr th {
-    background-color: #015289;
-    text-transform: capitalize;
-    color: white;
-  }
-
+tr th {
+  background-color: #015289;
+  text-transform: capitalize;
+  color: white;
+}
 </style>
