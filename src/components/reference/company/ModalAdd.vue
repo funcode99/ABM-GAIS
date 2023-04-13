d51fb7cbcbccf3e7fcc61a3826237ed87c7ed46e
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-13 17:36:36 +0700

    Update Responsive on Modal Page Company

--- src/components/reference/company/ModalAdd.vue
+++ src/components/reference/company/ModalAdd.vue
@@ -86,7 +86,7 @@ function removeLastTag(event) {
               >Parent Company<span class="text-red">*</span></label
             >
             <select
-              class="bg-white w-full lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
               required
             >
               <option disabled selected>Company</option>
