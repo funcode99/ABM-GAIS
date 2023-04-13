d596724ec8ba282dadf1a8e3047e3dbbbbd32dd1
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-14 00:07:24 +0700

    Update Style on Modal Page Site

--- src/components/reference/sites/ModalAdd.vue
+++ src/components/reference/sites/ModalAdd.vue
@@ -12,51 +12,52 @@ import iconClose from "@/assets/navbar/icon_close.svg";
   <input type="checkbox" id="my-modal-3" class="modal-toggle" />
   <div class="modal">
     <div class="modal-box relative">
-      <nav class="sticky top-0 z-50 bg-white py-4">
-        <label
-          for="my-modal-3"
-          class="cursor-pointer absolute right-0 top-2 lg:top-5"
-        >
+      <nav class="sticky top-0 z-50 bg-[#015289]">
+        <label for="my-modal-3" class="cursor-pointer absolute right-3 top-3">
           <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
         </label>
-        <p class="font-JakartaSans text-2xl font-semibold">New Site</p>
-        <div className="divider m-0"></div>
+        <p class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2">
+          New Site
+        </p>
       </nav>
 
-      <form>
-        <div class="mb-6">
-          <label
-            for="company"
-            class="block mb-2 font-JakartaSans font-medium text-sm"
-            id="company"
-            >Company<span class="text-red">*</span></label
-          >
-          <select class="select select-accent w-40" required>
-            <option disabled selected>Company</option>
-            <option>Company A</option>
-            <option>Company B</option>
-          </select>
-        </div>
-        <div class="mb-6">
-          <label
-            for="site"
-            class="block mb-2 font-JakartaSans font-medium text-sm"
-            id="site"
-            >Site Name<span class="text-red">*</span></label
-          >
-          <input
-            type="text"
-            id="site"
-            placeholder="Site Name"
-            class="input input-bordered input-accent w-full font-JakartaSans font-semibold text-base capitalize"
-            required
-          />
-        </div>
-      </form>
+      <main class="modal-box-inner">
+        <form class="pt-4">
+          <div class="mb-6">
+            <label
+              for="company"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Company<span class="text-red">*</span></label
+            >
+            <select
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              required
+            >
+              <option disabled selected>Company</option>
+              <option>Company A</option>
+              <option>Company B</option>
+            </select>
+          </div>
 
-      <div class="sticky bottom-0 bg-white py-4">
-        <div className="divider m-0 pb-4"></div>
-        <div class="flex justify-end gap-4">
+          <div class="mb-6 mr-6">
+            <label
+              for="site"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Site Name<span class="text-red">*</span></label
+            >
+            <input
+              type="text"
+              name="site"
+              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Site Name"
+              required
+            />
+          </div>
+        </form>
+      </main>
+
+      <div class="sticky bottom-0 bg-white py-2">
+        <div class="flex justify-end gap-4 mr-6">
           <label
             for="my-modal-3"
             class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
@@ -75,29 +76,19 @@ import iconClose from "@/assets/navbar/icon_close.svg";
 
 <style scoped>
 .modal-box {
-  max-height: calc(100vh - 5em);
-  --tw-bg-opacity: 1;
-  background-color: hsl(var(--b1) / var(--tw-bg-opacity));
-  padding-top: 0rem;
-  padding-bottom: 0rem;
-  transition-property: color, background-color, border-color,
-    text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter,
-    backdrop-filter;
-  transition-duration: 200ms;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  width: 91.666667%;
-  max-width: 32rem /* 512px */;
+  padding: 0;
+  overflow-y: hidden;
+  overscroll-behavior: contain;
+}
+
+.modal-box-inner {
   --tw-scale-x: 0.9;
   --tw-scale-y: 0.9;
   transform: translate(var(--tw-translate-x), var(--tw-translate-y))
     rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
     scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
-  border-top-left-radius: var(--rounded-box, 1rem);
-  border-top-right-radius: var(--rounded-box, 1rem);
-  border-bottom-left-radius: var(--rounded-box, 1rem);
-  border-bottom-right-radius: var(--rounded-box, 1rem);
-  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
   overflow-y: auto;
-  overscroll-behavior: contain;
+  overflow-x: hidden;
+  overscroll-behavior-y: contain;
 }
 </style>
