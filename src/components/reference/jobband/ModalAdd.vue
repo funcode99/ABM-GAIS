a9208a518c929f2497aabaf219cf84c1ef3b90a1
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-14 00:34:58 +0700

    Update Style Modal on Page Pagu

--- src/components/reference/jobband/ModalAdd.vue
+++ src/components/reference/jobband/ModalAdd.vue
@@ -1,5 +1,6 @@
 <script setup>
 import iconClose from "@/assets/navbar/icon_close.svg";
+import InnerTable from "../jobband/InnerTable.vue";
 </script>
 
 <template>
@@ -12,64 +13,75 @@ import iconClose from "@/assets/navbar/icon_close.svg";
   <input type="checkbox" id="my-modal-3" class="modal-toggle" />
   <div class="modal">
     <div class="modal-box relative">
-      <nav class="sticky top-0 z-50 bg-white py-4">
+      <nav class="sticky top-0 z-50 bg-[#015289]">
         <label
           for="my-modal-3"
-          class="cursor-pointer absolute right-0 top-2 lg:top-5"
+          class="cursor-pointer absolute right-3 top-0 lg:top-3"
         >
           <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
         </label>
-        <p class="font-JakartaSans text-2xl font-semibold">New Job Band</p>
-        <div className="divider m-0"></div>
+        <p
+          class="font-JakartaSans text-sm lg:text-2xl font-semibold text-white mx-4 py-2"
+        >
+          New Job Band
+        </p>
       </nav>
 
-      <form>
-        <div class="mb-6">
-          <label
-            for="job"
-            class="block mb-2 font-JakartaSans font-medium text-sm"
-            id="job"
-            >Job Band<span class="text-red">*</span></label
-          >
-          <input
-            type="text"
-            id="job"
-            placeholder="Job Band"
-            class="input input-bordered input-accent w-full font-JakartaSans font-semibold text-base capitalize"
-            required
-          />
-        </div>
-        <div class="mb-6">
-          <label
-            for="company"
-            class="block mb-2 font-JakartaSans font-medium text-sm"
-            id="company"
-            >Company<span class="text-red">*</span></label
-          >
-          <select class="select select-accent w-40" required>
-            <option disabled selected>Company</option>
-            <option>Company A</option>
-            <option>Company B</option>
-          </select>
-        </div>
-        <div class="mb-6">
-          <label
-            for="pagu"
-            class="block mb-2 font-JakartaSans font-medium text-sm"
-            id="pagu"
-            >Pagu<span class="text-red">*</span></label
-          >
-          <select class="select select-accent w-40" required>
-            <option disabled selected>Pagu</option>
-            <option>Pagu A</option>
-            <option>Pagu B</option>
-          </select>
-        </div>
-      </form>
+      <main class="modal-box-inner pb-14">
+        <form class="pt-4">
+          <div class="mb-6 mr-6">
+            <label
+              for="jobband"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Job Band<span class="text-red">*</span></label
+            >
+            <input
+              type="text"
+              name="jobband"
+              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Job Band"
+              required
+            />
+          </div>
 
-      <div class="sticky bottom-0 bg-white py-4">
-        <div className="divider m-0 pb-4"></div>
-        <div class="flex justify-end gap-4">
+          <div class="mb-6">
+            <label
+              for="company"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Company<span class="text-red">*</span></label
+            >
+            <select
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              required
+            >
+              <option disabled selected>Company</option>
+              <option>Company A</option>
+              <option>Company B</option>
+            </select>
+          </div>
+
+          <div class="mb-6">
+            <label
+              for="pagu"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Pagu<span class="text-red">*</span></label
+            >
+            <select
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              required
+            >
+              <option disabled selected>Pagu</option>
+              <option>Pagu A</option>
+              <option>Pagu B</option>
+            </select>
+          </div>
+
+          <InnerTable class="pb-4" />
+        </form>
+      </main>
+
+      <div class="sticky bottom-0 bg-white py-2">
+        <div class="flex justify-end gap-4 mr-6">
           <label
             for="my-modal-3"
             class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
@@ -88,29 +100,20 @@ import iconClose from "@/assets/navbar/icon_close.svg";
 
 <style scoped>
 .modal-box {
-  max-height: calc(100vh - 5em);
-  --tw-bg-opacity: 1;
-  background-color: hsl(var(--b1) / var(--tw-bg-opacity));
-  padding-top: 0rem;
-  padding-bottom: 0rem;
-  transition-property: color, background-color, border-color,
-    text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter,
-    backdrop-filter;
-  transition-duration: 200ms;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  width: 91.666667%;
-  max-width: 32rem /* 512px */;
+  padding: 0;
+  overflow-y: hidden;
+  overscroll-behavior: contain;
+}
+
+.modal-box-inner {
+  height: 500px;
   --tw-scale-x: 0.9;
   --tw-scale-y: 0.9;
   transform: translate(var(--tw-translate-x), var(--tw-translate-y))
     rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
     scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
-  border-top-left-radius: var(--rounded-box, 1rem);
-  border-top-right-radius: var(--rounded-box, 1rem);
-  border-bottom-left-radius: var(--rounded-box, 1rem);
-  border-bottom-right-radius: var(--rounded-box, 1rem);
-  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
   overflow-y: auto;
-  overscroll-behavior: contain;
+  overflow-x: hidden;
+  overscroll-behavior-y: contain;
 }
 </style>
