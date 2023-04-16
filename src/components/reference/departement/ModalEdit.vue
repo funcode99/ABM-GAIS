3b3b5be200acb3f617a6cd8f8dca2a5b5fd9a7b9
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-16 21:33:04 +0700

    Update on Page Departement

---
+++ src/components/reference/departement/ModalEdit.vue
@@ -0,0 +1,256 @@
+<script setup>
+import iconClose from "@/assets/navbar/icon_close.svg";
+import editicon from "@/assets/navbar/edit_icon.svg";
+
+//code for tags
+import { ref } from "vue";
+
+const tags = ref([]);
+
+function addTag(event) {
+  if (event.code === "Comma" || event.code === "Enter") {
+    event.preventDefault();
+    var val = event.target.value.trim();
+
+    if (val.length > 0) {
+      tags.value.push(val);
+      event.target.value = "";
+    }
+  }
+}
+
+function removeTag(index) {
+  tags.value.splice(index, 1);
+}
+
+function removeLastTag(event) {
+  if (event.target.value.length === 0) {
+    removeTag(tags.value.length - 1);
+  }
+}
+</script>
+
+<template>
+  <label for="modal-edit-departement" class="cursor-pointer">
+    <img :src="editicon" class="w-6 h-6"
+  /></label>
+
+  <input type="checkbox" id="modal-edit-departement" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box relative">
+      <nav class="sticky top-0 z-50 bg-[#015289]">
+        <label
+          for="modal-edit-departement"
+          class="cursor-pointer absolute right-3 top-3"
+        >
+          <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
+        </label>
+        <p
+          class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2 text-start"
+        >
+          Edit Departement
+        </p>
+      </nav>
+
+      <main class="modal-box-inner pb-14">
+        <form class="py-4">
+          <div class="w-full px-3">
+            <div class="mb-6 text-start">
+              <label
+                for="company"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Company<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Company</option>
+                <option>Company A</option>
+                <option>Company B</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="mb-6 mx-3 text-start">
+            <label
+              for="name"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Name<span class="text-red">*</span></label
+            >
+            <input
+              type="text"
+              name="name"
+              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Name Departement"
+              required
+            />
+          </div>
+
+          <div
+            class="flex justify-between lg:justify-around items-center gap-2 mx-3 lg:mx-0"
+          >
+            <div class="mb-6 text-start">
+              <label
+                for="cost_center"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Cost Center<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-[175px] lg:w-56 md:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Cost Center</option>
+                <option>Cost Center A</option>
+                <option>Cost Center B</option>
+              </select>
+            </div>
+            <div class="mb-6 text-start">
+              <label
+                for="status"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Status<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-[130px] lg:w-56 md:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Status</option>
+                <option>Active</option>
+                <option>Non Active</option>
+              </select>
+            </div>
+          </div>
+
+          <div
+            class="flex justify-between lg:justify-around items-center gap-2 mx-3 lg:mx-0"
+          >
+            <div class="mb-6 text-start">
+              <label
+                for="glaccount"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >GL Account<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-[175px] lg:w-56 md:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Account</option>
+                <option>Account A</option>
+                <option>Account B</option>
+              </select>
+            </div>
+            <div class="mb-6 text-start">
+              <label
+                for="profit"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Profit Center<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-[130px] lg:w-56 md:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Profit Center</option>
+                <option>Profit A</option>
+                <option>Profit B</option>
+              </select>
+            </div>
+          </div>
+
+          <div
+            class="flex justify-between lg:justify-around items-start gap-2 mx-3 lg:mx-0"
+          >
+            <div class="mb-6 text-start">
+              <label
+                for="Division"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Division<span class="text-red">*</span></label
+              >
+              <div
+                class="block bg-white w-full lg:w-[220px] md:w-56 border border-slate-300 rounded-md shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              >
+                <div
+                  v-for="(tag, index) in tags"
+                  :key="tag"
+                  class="h-[30px] bg-[#e4e4e4] my-1 rounded-md text-center items-center font-JakartaSans font-medium text-sm mx-2"
+                >
+                  <p class="pt-1">
+                    {{ tag }}
+                    <span
+                      @click="removeTag(index)"
+                      class="items-center cursor-pointer pl-10"
+                      >x</span
+                    >
+                  </p>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Input Division"
+                  class="tag-input__text px-2 text-sm font-medium leading-[38px] font-JakartaSans"
+                  @keydown="addTag"
+                  @keydown.delete="removeLastTag"
+                />
+              </div>
+            </div>
+
+            <div class="mb-6 text-start">
+              <label
+                for="departementhead"
+                class="block mb-2 font-JakartaSans font-medium text-sm"
+                >Departemen Head<span class="text-red">*</span></label
+              >
+              <select
+                class="bg-white w-full lg:w-56 md:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                required
+              >
+                <option disabled selected>Nama</option>
+                <option>Nama A</option>
+                <option>Nama B</option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </main>
+
+      <div class="sticky bottom-0 bg-white py-2">
+        <div class="flex justify-end gap-4 mr-6">
+          <label
+            for="modal-edit-departement"
+            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
+            >Cancel</label
+          >
+          <button
+            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-box {
+  padding: 0;
+  overflow-y: hidden;
+  overscroll-behavior: contain;
+}
+
+.modal-box-inner {
+  height: 500px;
+  --tw-scale-x: 0.9;
+  --tw-scale-y: 0.9;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y))
+    rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+  overflow-y: auto;
+  overflow-x: hidden;
+  overscroll-behavior-y: contain;
+}
+
+.tag-input__text {
+  border: none;
+  outline: none;
+}
+</style>
--- src/views/reference/departemen/ListDepartement.vue
+++ src/views/reference/departemen/ListDepartement.vue
@@ -1,23 +1,57 @@
 <script setup>
-import Layout from "@/components/layout/Layout.vue";
+import Navbar from "@/components/layout/Navbar.vue";
 import Sidebar from "@/components/layout/Sidebar.vue";
-import TableDepartement from "@/components/reference/departement/TableDepartement.vue";
 import Pagination from "@/components/reference/departement/Pagination.vue";
 import ModalAdd from "@/components/reference/departement/ModalAdd.vue";
+import ModalEdit from "@/components/reference/departement/ModalEdit.vue";
 import icon_filter from "@/assets/icon_filter.svg";
 import icon_reset from "@/assets/icon_reset.svg";
 import icon_receive from "@/assets/icon-receive.svg";
+import deleteicon from "@/assets/navbar/delete_icon.svg";
+import arrowicon from "@/assets/navbar/icon_arrow.svg";
+
+import departementdata from "@/utils/Api/reference/departementdata";
+
+//for check & uncheck all
+const selectAll = (checkValue) => {
+  const checkList = checkValue;
+  if (checkList == true) {
+    let check = document.getElementsByName("checks");
+    for (let i = 0; i < check.length; i++) {
+      if (check[i].type == "checkbox") check[i].checked = true;
+    }
+  } else {
+    let check = document.getElementsByName("checks");
+    for (let i = 0; i < check.length; i++) {
+      if (check[i].type == "checkbox") check[i].checked = false;
+    }
+  }
+};
+
+//for tablehead
+const tableHead = [
+  { Id: 1, title: "No" },
+  { Id: 2, title: "Name" },
+  { Id: 3, title: "Cost Center" },
+  { Id: 4, title: "Status" },
+  { Id: 5, title: "Departement Head" },
+  { Id: 6, title: "Actions" },
+];
 </script>
 
 <template>
-  <div class="flex overflow-y-hidden">
-    <Sidebar class="flex-none" />
+  <div class="flex flex-col basis-full grow-0 shrink-0 w-full this">
+    <Navbar />
 
-    <div class="card card-compact w-full bg-white rounded-lg">
-      <Layout />
+    <div class="flex w-screen mt-[115px]">
+      <Sidebar class="flex-none fixed" />
 
-      <div class="bg-slate-300 pt-5 px-5 h-[100%]">
-        <div class="bg-white rounded-xl custom-card">
+      <div
+        class="bg-slate-300 py-5 pl-5 pr-5 lg:pr-10 sm:ml-[100px] md:ml-[280px] w-screen h-full"
+        :class="[departementdata.length < 10 ? 'h-screen' : 'h-full']"
+      >
+        <div class="bg-white rounded-t-xl custom-card">
+          <!-- USER , EXPORT BUTTON, ADD NEW BUTTON -->
           <div
             class="grid grid-flow-col auto-cols-max items-center justify-between mx-4 py-2"
           >
@@ -36,6 +70,7 @@ import icon_receive from "@/assets/icon-receive.svg";
             </div>
           </div>
 
+          <!-- SORT & SEARCH -->
           <div class="flex flex-wrap justify-between items-center mx-4 py-2">
             <div class="flex flex-wrap items-center gap-4">
               <p
@@ -117,6 +152,7 @@ import icon_receive from "@/assets/icon-receive.svg";
             </form>
           </div>
 
+          <!-- SHOWING -->
           <div class="flex items-center gap-1 pt-2 pb-4 px-4 h-4">
             <h1 class="text-xs font-JakartaSans">Showing</h1>
             <select class="border-2 border-black rounded-lg w-15" name="" id="">
@@ -128,8 +164,64 @@ import icon_receive from "@/assets/icon-receive.svg";
             </select>
           </div>
 
-          <TableDepartement class="py-2 mx-4 overflow-x-auto" />
+          <!-- TABLE -->
+          <div
+            class="px-4 py-2 bg-white rounded-b-xl box-border block overflow-x-hidden"
+          >
+            <div class="block overflow-x-auto">
+              <table
+                class="table table-zebra table-compact border w-full rounded-lg"
+              >
+                <thead class="text-center font-JakartaSans text-sm font-bold">
+                  <tr>
+                    <th class="relative">
+                      <div class="flex justify-center">
+                        <input
+                          type="checkbox"
+                          @click="selectAll((checkList = !checkList))"
+                        />
+                      </div>
+                    </th>
+                    <th
+                      v-for="data in tableHead"
+                      :key="data.Id"
+                      class="relative"
+                    >
+                      <span class="flex justify-center">{{ data.title }}</span>
+                      <button class="absolute right-2 top-0 bottom-0">
+                        <img :src="arrowicon" class="w-[9px] h-3" />
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
 
+                <tbody class="bg-[#F5F5F5]">
+                  <tr
+                    class="font-JakartaSans font-normal text-sm"
+                    v-for="(data, index) in departementdata"
+                    :key="index"
+                  >
+                    <td class="relative">
+                      <input type="checkbox" name="checks" />
+                    </td>
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ data.name_departement }}</td>
+                    <td>{{ data.cost_center }}</td>
+                    <td>{{ data.status }}</td>
+                    <td>{{ data.name_departement_head }}</td>
+                    <td class="flex flex-wrap gap-4 justify-center">
+                      <ModalEdit />
+                      <button>
+                        <img :src="deleteicon" class="w-6 h-6" />
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <!-- PAGINATION -->
           <div
             class="flex flex-wrap justify-center lg:justify-between items-center mx-4 py-2"
           >
@@ -148,5 +240,31 @@ import icon_receive from "@/assets/icon-receive.svg";
 .custom-card {
   box-shadow: 0px -4px #015289;
   border-radius: 4px;
+}
+
+th {
+  padding: 2px;
+  text-align: left;
+  position: relative;
+}
+
+tr td {
+  text-align: center;
+  white-space: nowrap;
+}
+
+tr th {
+  background-color: #015289;
+  text-transform: capitalize;
+  color: white;
+}
+
+.table-zebra tbody tr:hover td {
+  background-color: rgb(193, 192, 192);
+  cursor: pointer;
+}
+
+.this {
+  overflow-x: hidden;
 }
 </style>
