0e2b81405c1340d14345d1ff2ea7d0c91de05715
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 10:40:05 +0700

    Update on Page Pagu

---
+++ src/utils/Api/reference/pagudata.js
@@ -0,0 +1,129 @@
+const paguData = [
+    {
+        id: 1,
+        hotel_fare: 150000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 2,
+        hotel_fare: 200000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 3,
+        hotel_fare: 175000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 4,
+        hotel_fare: 220000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 5,
+        hotel_fare: 190000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 6,
+        hotel_fare: 205000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 7,
+        hotel_fare: 165000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 8,
+        hotel_fare: 195000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 9,
+        hotel_fare: 185000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 10,
+        hotel_fare: 210000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 11,
+        hotel_fare: 160000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 12,
+        hotel_fare: 180000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 13,
+        hotel_fare: 165000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 14,
+        hotel_fare: 225000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 15,
+        hotel_fare: 185000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 16,
+        hotel_fare: 195000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 17,
+        hotel_fare: 175000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 18,
+        hotel_fare: 220000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 19,
+        hotel_fare: 190000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 20,
+        hotel_fare: 205000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 21,
+        hotel_fare: 160000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 22,
+        hotel_fare: 180000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 23,
+        hotel_fare: 170000,
+        company: 'ABC Corp'
+    },
+    {
+        id: 24,
+        hotel_fare: 210000,
+        company: 'XYZ Inc'
+    },
+    {
+        id: 25,
+        hotel_fare: 195000,
+        company: 'ABC Corp'
+    },
+]
+
+export default paguData
