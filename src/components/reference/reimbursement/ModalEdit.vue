4e7da9ce824a77864a51ef5d07ba245a3c1d666e
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 11:19:04 +0700

    Update on Page Reimbursement

---
+++ src/components/reference/reimbursement/ModalEdit.vue
@@ -0,0 +1,114 @@
+<script setup>
+import iconClose from "@/assets/navbar/icon_close.svg";
+import editicon from "@/assets/navbar/edit_icon.svg";
+</script>
+
+<template>
+  <label for="modal-edit-reimbursement" class="cursor-pointer"
+    ><img :src="editicon" class="w-6 h-6"
+  /></label>
+
+  <input type="checkbox" id="modal-edit-reimbursement" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box relative">
+      <nav class="sticky top-0 z-50 bg-[#015289]">
+        <label
+          for="modal-edit-reimbursement"
+          class="cursor-pointer absolute right-3 top-0 lg:top-3"
+        >
+          <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
+        </label>
+        <p
+          class="font-JakartaSans text-sm lg:text-2xl font-semibold text-white mx-4 py-2 text-start"
+        >
+          Edit Reimbursement Type
+        </p>
+      </nav>
+
+      <main class="modal-box-inner">
+        <form class="pt-4">
+          <div class="mb-6 mr-6 text-start">
+            <label
+              for="reimbursement_type"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Reimbursement Type<span class="text-red">*</span></label
+            >
+            <input
+              type="text"
+              name="reimbursement_type"
+              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Reimbursement Type"
+              required
+            />
+          </div>
+
+          <div class="mb-6 text-start">
+            <label
+              for="parent_type"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Parent Type<span class="text-red">*</span></label
+            >
+            <select
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              required
+            >
+              <option disabled selected>Type</option>
+              <option>Type A</option>
+              <option>Type B</option>
+            </select>
+          </div>
+
+          <div class="mb-6 text-start">
+            <label
+              for="company"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Company<span class="text-red">*</span></label
+            >
+            <select
+              class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+              required
+            >
+              <option disabled selected>Company</option>
+              <option>Company A</option>
+              <option>Company B</option>
+            </select>
+          </div>
+        </form>
+      </main>
+
+      <div class="sticky bottom-0 bg-white py-2">
+        <div class="flex justify-end gap-4 mr-6">
+          <label
+            for="modal-edit-reimbursement"
+            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
+            >Cancel</label
+          >
+          <button
+            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-box {
+  padding: 0;
+  overflow-y: hidden;
+  overscroll-behavior: contain;
+}
+
+.modal-box-inner {
+  --tw-scale-x: 0.9;
+  --tw-scale-y: 0.9;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y))
+    rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+  overflow-y: auto;
+  overflow-x: hidden;
+  overscroll-behavior-y: contain;
+}
+</style>
---
+++ src/utils/Api/reference/reimbursementdata.js
@@ -0,0 +1,154 @@
+const reimbursementData = [
+    {
+        id: 1,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 2,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 3,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 4,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 5,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 6,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 7,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 8,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 9,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 10,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 11,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 12,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 13,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 14,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 15,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 16,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 17,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 18,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 19,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 20,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 21,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 22,
+        reimbursement_type: 'Meals',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 23,
+        reimbursement_type: 'Flight',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+    {
+        id: 24,
+        reimbursement_type: 'Transportation',
+        parent_type: 'Travel',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 25,
+        reimbursement_type: 'Hotel',
+        parent_type: 'Travel',
+        company: 'ABC Corp'
+    },
+]
+
+export default reimbursementData
