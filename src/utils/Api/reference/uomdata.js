92f0e4f7ca1f516453c0dcdccd0d01218ad61905
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 21:44:13 +0700

    Added Sort & Search on Page UOM

--- src/utils/Api/reference/uomdata.js
+++ src/utils/Api/reference/uomdata.js
@@ -1,150 +1,175 @@
 const uomData = [
     {
         id: 1,
+        no: 1,
         uom_category: 'Length',
         uom: 'Meter',
         company: 'ABC Corp'
     },
     {
         id: 2,
+        no: 2,
         uom_category: 'Length',
         uom: 'Centimeter',
         company: 'XYZ Inc'
     },
     {
         id: 3,
+        no: 3,
         uom_category: 'Length',
         uom: 'Inch',
         company: 'ABC Corp'
     },
     {
         id: 4,
+        no: 4,
         uom_category: 'Weight',
         uom: 'Kilogram',
         company: 'XYZ Inc'
     },
     {
         id: 5,
+        no: 5,
         uom_category: 'Weight',
         uom: 'Pound',
         company: 'ABC Corp'
     },
     {
         id: 6,
+        no: 6,
         uom_category: 'Weight',
         uom: 'Gram',
         company: 'XYZ Inc'
     },
     {
         id: 7,
+        no: 7,
         uom_category: 'Volume',
         uom: 'Liter',
         company: 'ABC Corp'
     },
     {
         id: 8,
+        no: 8,
         uom_category: 'Volume',
         uom: 'Gallon',
         company: 'XYZ Inc'
     },
     {
         id: 9,
+        no: 9,
         uom_category: 'Volume',
         uom: 'Fluid Ounce',
         company: 'ABC Corp'
     },
     {
         id: 10,
+        no: 10,
         uom_category: 'Temperature',
         uom: 'Celsius',
         company: 'XYZ Inc'
     },
     {
         id: 11,
+        no: 11,
         uom_category: 'Temperature',
         uom: 'Fahrenheit',
         company: 'ABC Corp'
     },
     {
         id: 12,
+        no: 12,
         uom_category: 'Area',
         uom: 'Square Meter',
         company: 'XYZ Inc'
     },
     {
         id: 13,
+        no: 13,
         uom_category: 'Area',
         uom: 'Square Foot',
         company: 'ABC Corp'
     },
     {
         id: 14,
+        no: 14,
         uom_category: 'Area',
         uom: 'Hectare',
         company: 'XYZ Inc'
     },
     {
         id: 15,
+        no: 15,
         uom_category: 'Speed',
         uom: 'Kilometer per hour',
         company: 'ABC Corp'
     },
     {
         id: 16,
+        no: 16,
         uom_category: 'Speed',
         uom: 'Mile per hour',
         company: 'XYZ Inc'
     },
     {
         id: 17,
+        no: 17,
         uom_category: 'Frequency',
         uom: 'Hertz',
         company: 'ABC Corp'
     },
     {
         id: 18,
+        no: 18,
         uom_category: 'Frequency',
         uom: 'Megahertz',
         company: 'XYZ Inc'
     },
     {
         id: 19,
+        no: 19,
         uom_category: 'Energy',
         uom: 'Joule',
         company: 'ABC Corp'
     },
     {
         id: 20,
+        no: 20,
         uom_category: 'Energy',
         uom: 'Calorie',
         company: 'XYZ Inc'
     },
     {
         id: 21,
+        no: 21,
         uom_category: 'Pressure',
         uom: 'Pascal',
         company: 'ABC Corp'
     },
     {
         id: 22,
+        no: 22,
         uom_category: 'Pressure',
         uom: 'Bar',
         company: 'XYZ Inc'
     },
     {
         id: 23,
+        no: 23,
         uom_category: 'Power',
         uom: 'Watt',
         company: 'ABC Corp'
     },
     {
         id: 24,
+        no: 24,
         uom_category: 'Power',
         uom: 'Horsepower',
         company: 'XYZ Inc'
     },
     {
         id: 25,
+        no: 25,
         uom_category: 'Data Size',
         uom: 'Byte',
         company: 'ABC Corp'
