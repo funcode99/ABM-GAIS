efb6d5d1cda288ca3dc9a06d1541903da830e82b
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 21:25:11 +0700

    Added Sort & Search in page Reimbursement

--- src/utils/Api/reference/reimbursementdata.js
+++ src/utils/Api/reference/reimbursementdata.js
@@ -1,150 +1,175 @@
 const reimbursementData = [
     {
         id: 1,
+        no: 1,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 2,
+        no: 2,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 3,
+        no: 3,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 4,
+        no: 4,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 5,
+        no: 5,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 6,
+        no: 6,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 7,
+        no: 7,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 8,
+        no: 8,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 9,
+        no: 9,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 10,
+        no: 10,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 11,
+        no: 11,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 12,
+        no: 12,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 13,
+        no: 13,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 14,
+        no: 14,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 15,
+        no: 15,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 16,
+        no: 16,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 17,
+        no: 17,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 18,
+        no: 18,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 19,
+        no: 19,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 20,
+        no: 20,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 21,
+        no: 21,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 22,
+        no: 22,
         reimbursement_type: 'Meals',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 23,
+        no: 23,
         reimbursement_type: 'Flight',
         parent_type: 'Travel',
         company: 'ABC Corp'
     },
     {
         id: 24,
+        no: 24,
         reimbursement_type: 'Transportation',
         parent_type: 'Travel',
         company: 'XYZ Inc'
     },
     {
         id: 25,
+        no: 25,
         reimbursement_type: 'Hotel',
         parent_type: 'Travel',
         company: 'ABC Corp'
--- src/views/reference/reimbursement/ListReimbursement.vue
+++ src/views/reference/reimbursement/ListReimbursement.vue
@@ -12,6 +12,13 @@ import arrowicon from "@/assets/navbar/icon_arrow.svg";
 
 import reimbursementdata from "@/utils/Api/reference/reimbursementdata";
 
+import { ref, onMounted, onBeforeMount, reactive, computed } from "vue";
+
+const search = ref("");
+let sortedData = ref([]);
+let sortedbyASC = true;
+let instanceArray = [];
+
 //for check & uncheck all
 const selectAll = (checkValue) => {
   const checkList = checkValue;
@@ -30,12 +37,43 @@ const selectAll = (checkValue) => {
 
 //for tablehead
 const tableHead = [
-  { Id: 1, title: "No" },
-  { Id: 2, title: "Reimbursement Type" },
-  { Id: 3, title: "Parent Type" },
-  { Id: 4, title: "Company" },
+  { Id: 1, title: "No", jsonData: "no" },
+  { Id: 2, title: "Reimbursement Type", jsonData: "reimbursement_type" },
+  { Id: 3, title: "Parent Type", jsonData: "parent_type" },
+  { Id: 4, title: "Company", jsonData: "company" },
   { Id: 5, title: "Actions" },
 ];
+
+//for sort
+const sortList = (sortBy) => {
+  if (sortedbyASC) {
+    sortedData.value.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
+    sortedbyASC = false;
+  } else {
+    sortedData.value.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
+    sortedbyASC = true;
+  }
+};
+
+onBeforeMount(() => {
+  instanceArray = reimbursementdata;
+  sortedData.value = instanceArray;
+});
+
+//for searching
+const filteredItems = (search) => {
+  sortedData.value = instanceArray;
+  const filteredR = sortedData.value.filter((item) => {
+    (item.reimbursement_type.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    return (
+      (item.reimbursement_type.toLowerCase().indexOf(search.toLowerCase()) >
+        -1) |
+      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1)
+    );
+  });
+  sortedData.value = filteredR;
+};
 </script>
 
 <template>
@@ -121,7 +159,7 @@ const tableHead = [
               </div>
             </div>
 
-            <form class="py-2 flex md:mx-0">
+            <div class="py-2 flex md:mx-0">
               <label class="relative block">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                   <svg
@@ -141,13 +179,15 @@ const tableHead = [
                   </svg>
                 </span>
                 <input
-                  class="placeholder:text-slate-400 placeholder:font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                  placeholder="Search..."
+                  class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-xs capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                  placeholder="Search by Type / Company"
                   type="text"
                   name="search"
+                  v-model="search"
+                  @keyup="filteredItems(search)"
                 />
               </label>
-            </form>
+            </div>
           </div>
 
           <!-- SHOWING -->
@@ -184,6 +224,7 @@ const tableHead = [
                       v-for="data in tableHead"
                       :key="data.Id"
                       class="relative"
+                      @click="sortList(`${data.jsonData}`)"
                     >
                       <span class="flex justify-center">{{ data.title }}</span>
                       <button class="absolute right-2 top-0 bottom-0">
@@ -196,13 +237,13 @@ const tableHead = [
                 <tbody class="bg-[#F5F5F5]">
                   <tr
                     class="font-JakartaSans font-normal text-sm"
-                    v-for="(data, index) in reimbursementdata"
-                    :key="index"
+                    v-for="data in sortedData"
+                    :key="data.no"
                   >
                     <td class="relative">
                       <input type="checkbox" name="checks" />
                     </td>
-                    <td>{{ index + 1 }}</td>
+                    <td>{{ data.no }}</td>
                     <td>{{ data.reimbursement_type }}</td>
                     <td>{{ data.parent_type }}</td>
                     <td>{{ data.company }}</td>
