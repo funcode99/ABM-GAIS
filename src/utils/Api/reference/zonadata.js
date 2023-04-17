2931df8117f5adaf2786c313041ee62156c1b5c2
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 12:47:58 +0700

    Update on Page Zona

---
+++ src/utils/Api/reference/zonadata.js
@@ -0,0 +1,129 @@
+const zonaData = [
+    {
+        id: 1,
+        tlk: 'TLK-001',
+        company: 'ABC Corp'
+    },
+    {
+        id: 2,
+        tlk: 'TLK-002',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 3,
+        tlk: 'TLK-003',
+        company: 'ABC Corp'
+    },
+    {
+        id: 4,
+        tlk: 'TLK-004',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 5,
+        tlk: 'TLK-005',
+        company: 'ABC Corp'
+    },
+    {
+        id: 6,
+        tlk: 'TLK-006',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 7,
+        tlk: 'TLK-007',
+        company: 'ABC Corp'
+    },
+    {
+        id: 8,
+        tlk: 'TLK-008',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 9,
+        tlk: 'TLK-009',
+        company: 'ABC Corp'
+    },
+    {
+        id: 10,
+        tlk: 'TLK-010',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 11,
+        tlk: 'TLK-011',
+        company: 'ABC Corp'
+    },
+    {
+        id: 12,
+        tlk: 'TLK-012',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 13,
+        tlk: 'TLK-013',
+        company: 'ABC Corp'
+    },
+    {
+        id: 14,
+        tlk: 'TLK-014',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 15,
+        tlk: 'TLK-015',
+        company: 'ABC Corp'
+    },
+    {
+        id: 16,
+        tlk: 'TLK-016',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 17,
+        tlk: 'TLK-017',
+        company: 'ABC Corp'
+    },
+    {
+        id: 18,
+        tlk: 'TLK-018',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 19,
+        tlk: 'TLK-019',
+        company: 'ABC Corp'
+    },
+    {
+        id: 20,
+        tlk: 'TLK-020',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 21,
+        tlk: 'TLK-021',
+        company: 'ABC Corp'
+    },
+    {
+        id: 22,
+        tlk: 'TLK-022',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 23,
+        tlk: 'TLK-023',
+        company: 'ABC Corp'
+    },
+    {
+        id: 24,
+        tlk: 'TLK-024',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 25,
+        tlk: 'TLK-025',
+        company: 'ABC Corp'
+    },
+]
+
+export default zonaData
