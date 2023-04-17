4653574bf4f80b4d50eb86abc442400c243d992e
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 20:55:30 +0700

    Added Sort & Search in Page Job Band

--- src/utils/Api/reference/jobdata.js
+++ src/utils/Api/reference/jobdata.js
@@ -1,126 +1,151 @@
 const JobData = [
     {
         id: 1,
+        no: 1,
         job_band: 'Entry Level',
         company: 'ABC Corporation'
     },
     {
         id: 2,
+        no: 2,
         job_band: 'Junior Level',
         company: 'XYZ Inc.'
     },
     {
         id: 3,
+        no: 3,
         job_band: 'Mid Level',
         company: '123 Corporation'
     },
     {
         id: 4,
+        no: 4,
         job_band: 'Senior Level',
         company: 'DEF Inc.'
     },
     {
         id: 5,
+        no: 5,
         job_band: 'Executive Level',
         company: '456 Corporation'
     },
     {
         id: 6,
+        no: 6,
         job_band: 'Entry Level',
         company: 'GHI Inc.'
     },
     {
         id: 7,
+        no: 7,
         job_band: 'Junior Level',
         company: '789 Corporation'
     },
     {
         id: 8,
+        no: 8,
         job_band: 'Mid Level',
         company: 'JKL Inc.'
     },
     {
         id: 9,
+        no: 9,
         job_band: 'Senior Level',
         company: '012 Corporation'
     },
     {
         id: 10,
+        no: 10,
         job_band: 'Executive Level',
         company: 'MNO Inc.'
     },
     {
         id: 11,
+        no: 11,
         job_band: 'Entry Level',
         company: 'PQR Corporation'
     },
     {
         id: 12,
+        no: 12,
         job_band: 'Junior Level',
         company: 'STU Inc.'
     },
     {
         id: 13,
+        no: 13,
         job_band: 'Mid Level',
         company: '345 Corporation'
     },
     {
         id: 14,
+        no: 14,
         job_band: 'Senior Level',
         company: 'VWX Inc.'
     },
     {
         id: 15,
+        no: 15,
         job_band: 'Executive Level',
         company: '678 Corporation'
     },
     {
         id: 16,
+        no: 16,
         job_band: 'Entry Level',
         company: 'YZA Inc.'
     },
     {
         id: 17,
+        no: 17,
         job_band: 'Junior Level',
         company: 'BCD Corporation'
     },
     {
         id: 18,
+        no: 18,
         job_band: 'Mid Level',
         company: 'EFG Inc.'
     },
     {
         id: 19,
+        no: 19,
         job_band: 'Senior Level',
         company: 'HIJ Corporation'
     },
     {
         id: 20,
+        no: 20,
         job_band: 'Executive Level',
         company: 'KLM Inc.'
     },
     {
         id: 21,
+        no: 21,
         job_band: 'Entry Level',
         company: 'NOP Corporation'
     },
     {
         id: 22,
+        no: 22,
         job_band: 'Junior Level',
         company: 'QRS Inc.'
     },
     {
         id: 23,
+        no: 23,
         job_band: 'Mid Level',
         company: 'TUV Corporation'
     },
     {
         id: 24,
+        no: 24,
         job_band: 'Senior Level',
         company: 'WXY Inc.'
     },
     {
         id: 25,
+        no: 25,
         job_band: 'Executive Level',
         company: 'ZAB Corporation'
     },
