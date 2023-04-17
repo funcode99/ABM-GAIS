47dd28992694d2b48e5b56274a1e6320445b8442
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 21:32:57 +0700

    Added Sort & Search on Page Site

--- src/utils/Api/reference/sitedata.js
+++ src/utils/Api/reference/sitedata.js
@@ -1,126 +1,151 @@
 const siteData = [
     {
         id: 1,
+        no: 1,
         site: 'Site A',
         company: 'ABC Corp'
     },
     {
         id: 2,
+        no: 2,
         site: 'Site B',
         company: 'XYZ Inc'
     },
     {
         id: 3,
+        no: 3,
         site: 'Site C',
         company: 'ABC Corp'
     },
     {
         id: 4,
+        no: 4,
         site: 'Site D',
         company: 'XYZ Inc'
     },
     {
         id: 5,
+        no: 5,
         site: 'Site E',
         company: 'ABC Corp'
     },
     {
         id: 6,
+        no: 6,
         site: 'Site F',
         company: 'XYZ Inc'
     },
     {
         id: 7,
+        no: 7,
         site: 'Site G',
         company: 'ABC Corp'
     },
     {
         id: 8,
+        no: 8,
         site: 'Site H',
         company: 'XYZ Inc'
     },
     {
         id: 9,
+        no: 9,
         site: 'Site I',
         company: 'ABC Corp'
     },
     {
         id: 10,
+        no: 10,
         site: 'Site J',
         company: 'XYZ Inc'
     },
     {
         id: 11,
+        no: 11,
         site: 'Site K',
         company: 'ABC Corp'
     },
     {
         id: 12,
+        no: 12,
         site: 'Site L',
         company: 'XYZ Inc'
     },
     {
         id: 13,
+        no: 13,
         site: 'Site M',
         company: 'ABC Corp'
     },
     {
         id: 14,
+        no: 14,
         site: 'Site N',
         company: 'XYZ Inc'
     },
     {
         id: 15,
+        no: 15,
         site: 'Site O',
         company: 'ABC Corp'
     },
     {
         id: 16,
+        no: 16,
         site: 'Site P',
         company: 'XYZ Inc'
     },
     {
         id: 17,
+        no: 17,
         site: 'Site Q',
         company: 'ABC Corp'
     },
     {
         id: 18,
+        no: 18,
         site: 'Site R',
         company: 'XYZ Inc'
     },
     {
         id: 19,
+        no: 19,
         site: 'Site S',
         company: 'ABC Corp'
     },
     {
         id: 20,
+        no: 20,
         site: 'Site T',
         company: 'XYZ Inc'
     },
     {
         id: 21,
+        no: 21,
         site: 'Site U',
         company: 'ABC Corp'
     },
     {
         id: 22,
+        no: 22,
         site: 'Site V',
         company: 'XYZ Inc'
     },
     {
         id: 23,
+        no: 23,
         site: 'Site W',
         company: 'ABC Corp'
     },
     {
         id: 24,
+        no: 24,
         site: 'Site X',
         company: 'XYZ Inc'
     },
     {
         id: 25,
+        no: 25,
         site: 'Site Y',
         company: 'ABC Corp'
     },
