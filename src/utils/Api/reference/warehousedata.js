1fac6bac9d6cbaa51fa9b680130e5e57216b4529
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 21:50:57 +0700

    Added Sort & Search on Page Warehouse

--- src/utils/Api/reference/warehousedata.js
+++ src/utils/Api/reference/warehousedata.js
@@ -1,150 +1,175 @@
 const warehouseData = [
     {
         id: 1,
+        no: 1,
         warehouse_name: 'Warehouse A',
         company: 'ABC Corp',
         site: 'Site 1'
     },
     {
         id: 2,
+        no: 2,
         warehouse_name: 'Warehouse B',
         company: 'XYZ Inc',
         site: 'Site 2'
     },
     {
         id: 3,
+        no: 3,
         warehouse_name: 'Warehouse C',
         company: 'ABC Corp',
         site: 'Site 3'
     },
     {
         id: 4,
+        no: 4,
         warehouse_name: 'Warehouse D',
         company: 'XYZ Inc',
         site: 'Site 1'
     },
     {
         id: 5,
+        no: 5,
         warehouse_name: 'Warehouse E',
         company: 'ABC Corp',
         site: 'Site 2'
     },
     {
         id: 6,
+        no: 6,
         warehouse_name: 'Warehouse F',
         company: 'XYZ Inc',
         site: 'Site 3'
     },
     {
         id: 7,
+        no: 7,
         warehouse_name: 'Warehouse G',
         company: 'ABC Corp',
         site: 'Site 1'
     },
     {
         id: 8,
+        no: 8,
         warehouse_name: 'Warehouse H',
         company: 'XYZ Inc',
         site: 'Site 2'
     },
     {
         id: 9,
+        no: 9,
         warehouse_name: 'Warehouse I',
         company: 'ABC Corp',
         site: 'Site 3'
     },
     {
         id: 10,
+        no: 10,
         warehouse_name: 'Warehouse J',
         company: 'XYZ Inc',
         site: 'Site 1'
     },
     {
         id: 11,
+        no: 11,
         warehouse_name: 'Warehouse K',
         company: 'ABC Corp',
         site: 'Site 2'
     },
     {
         id: 12,
+        no: 12,
         warehouse_name: 'Warehouse L',
         company: 'XYZ Inc',
         site: 'Site 3'
     },
     {
         id: 13,
+        no: 13,
         warehouse_name: 'Warehouse M',
         company: 'ABC Corp',
         site: 'Site 1'
     },
     {
         id: 14,
+        no: 14,
         warehouse_name: 'Warehouse N',
         company: 'XYZ Inc',
         site: 'Site 2'
     },
     {
         id: 15,
+        no: 15,
         warehouse_name: 'Warehouse O',
         company: 'ABC Corp',
         site: 'Site 3'
     },
     {
         id: 16,
+        no: 16,
         warehouse_name: 'Warehouse P',
         company: 'XYZ Inc',
         site: 'Site 1'
     },
     {
         id: 17,
+        no: 17,
         warehouse_name: 'Warehouse Q',
         company: 'ABC Corp',
         site: 'Site 2'
     },
     {
         id: 18,
+        no: 18,
         warehouse_name: 'Warehouse R',
         company: 'XYZ Inc',
         site: 'Site 3'
     },
     {
         id: 19,
+        no: 19,
         warehouse_name: 'Warehouse S',
         company: 'ABC Corp',
         site: 'Site 1'
     },
     {
         id: 20,
+        no: 20,
         warehouse_name: 'Warehouse T',
         company: 'XYZ Inc',
         site: 'Site 2'
     },
     {
         id: 21,
+        no: 21,
         warehouse_name: 'Warehouse U',
         company: 'ABC Corp',
         site: 'Site 3'
     },
     {
         id: 22,
+        no: 22,
         warehouse_name: 'Warehouse V',
         company: 'XYZ Inc',
         site: 'Site 1'
     },
     {
         id: 23,
+        no: 23,
         warehouse_name: 'Warehouse X',
         company: 'XYZ Inc',
         site: 'Site 2'
     },
     {
         id: 24,
+        no: 24,
         warehouse_name: 'Warehouse Y',
         company: 'ABC Corp',
         site: 'Site 3'
     },
     {
         id: 25,
+        no: 25,
         warehouse_name: 'Warehouse Z',
         company: 'XYZ Inc',
         site: 'Site 1'
--- src/views/reference/warehouse/ListWarehouse.vue
+++ src/views/reference/warehouse/ListWarehouse.vue
@@ -13,6 +13,13 @@ import arrowicon from "@/assets/navbar/icon_arrow.svg";
 
 import warehousedata from "@/utils/Api/reference/warehousedata";
 
+import { ref, onMounted, onBeforeMount, reactive, computed } from "vue";
+
+const search = ref("");
+let sortedData = ref([]);
+let sortedbyASC = true;
+let instanceArray = [];
+
 //for check & uncheck all
 const selectAll = (checkValue) => {
   const checkList = checkValue;
@@ -31,12 +38,42 @@ const selectAll = (checkValue) => {
 
 //for tablehead
 const tableHead = [
-  { Id: 1, title: "No" },
-  { Id: 2, title: "Warehouse Name" },
-  { Id: 3, title: "Company" },
-  { Id: 4, title: "Site" },
+  { Id: 1, title: "No", jsonData: "no" },
+  { Id: 2, title: "Warehouse Name", jsonData: "warehouse_name" },
+  { Id: 3, title: "Company", jsonData: "company" },
+  { Id: 4, title: "Site", jsonData: "site" },
   { Id: 4, title: "Actions" },
 ];
+
+//for sort
+const sortList = (sortBy) => {
+  if (sortedbyASC) {
+    sortedData.value.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
+    sortedbyASC = false;
+  } else {
+    sortedData.value.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
+    sortedbyASC = true;
+  }
+};
+
+onBeforeMount(() => {
+  instanceArray = warehousedata;
+  sortedData.value = instanceArray;
+});
+
+//for searching
+const filteredItems = (search) => {
+  sortedData.value = instanceArray;
+  const filteredR = sortedData.value.filter((item) => {
+    (item.warehouse_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    return (
+      (item.warehouse_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1)
+    );
+  });
+  sortedData.value = filteredR;
+};
 </script>
 
 <template>
@@ -122,7 +159,7 @@ const tableHead = [
               </div>
             </div>
 
-            <form class="py-2 flex md:mx-0">
+            <div class="py-2 flex md:mx-0">
               <label class="relative block">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                   <svg
@@ -142,13 +179,15 @@ const tableHead = [
                   </svg>
                 </span>
                 <input
-                  class="placeholder:text-slate-400 placeholder:font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                  placeholder="Search..."
+                  class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-xs capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                  placeholder="Search by Name / Company"
                   type="text"
                   name="search"
+                  v-model="search"
+                  @keyup="filteredItems(search)"
                 />
               </label>
-            </form>
+            </div>
           </div>
 
           <!-- SHOWING -->
@@ -185,6 +224,7 @@ const tableHead = [
                       v-for="data in tableHead"
                       :key="data.Id"
                       class="relative"
+                      @click="sortList(`${data.jsonData}`)"
                     >
                       <span class="flex justify-center">{{ data.title }}</span>
                       <button class="absolute right-2 top-0 bottom-0">
@@ -197,13 +237,13 @@ const tableHead = [
                 <tbody class="bg-[#F5F5F5]">
                   <tr
                     class="font-JakartaSans font-normal text-sm"
-                    v-for="(data, index) in warehousedata"
-                    :key="index"
+                    v-for="data in sortedData"
+                    :key="data.no"
                   >
                     <td class="relative">
                       <input type="checkbox" name="checks" />
                     </td>
-                    <td>{{ index + 1 }}</td>
+                    <td>{{ data.no }}</td>
                     <td>{{ data.warehouse_name }}</td>
                     <td>{{ data.company }}</td>
                     <td>{{ data.site }}</td>
