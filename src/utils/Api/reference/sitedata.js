c6077585d621e56dace13a42912bbb886bac4c89
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 11:58:31 +0700

    Update on Page Site

---
+++ src/utils/Api/reference/sitedata.js
@@ -0,0 +1,129 @@
+const siteData = [
+    {
+        id: 1,
+        site: 'Site A',
+        company: 'ABC Corp'
+    },
+    {
+        id: 2,
+        site: 'Site B',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 3,
+        site: 'Site C',
+        company: 'ABC Corp'
+    },
+    {
+        id: 4,
+        site: 'Site D',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 5,
+        site: 'Site E',
+        company: 'ABC Corp'
+    },
+    {
+        id: 6,
+        site: 'Site F',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 7,
+        site: 'Site G',
+        company: 'ABC Corp'
+    },
+    {
+        id: 8,
+        site: 'Site H',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 9,
+        site: 'Site I',
+        company: 'ABC Corp'
+    },
+    {
+        id: 10,
+        site: 'Site J',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 11,
+        site: 'Site K',
+        company: 'ABC Corp'
+    },
+    {
+        id: 12,
+        site: 'Site L',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 13,
+        site: 'Site M',
+        company: 'ABC Corp'
+    },
+    {
+        id: 14,
+        site: 'Site N',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 15,
+        site: 'Site O',
+        company: 'ABC Corp'
+    },
+    {
+        id: 16,
+        site: 'Site P',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 17,
+        site: 'Site Q',
+        company: 'ABC Corp'
+    },
+    {
+        id: 18,
+        site: 'Site R',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 19,
+        site: 'Site S',
+        company: 'ABC Corp'
+    },
+    {
+        id: 20,
+        site: 'Site T',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 21,
+        site: 'Site U',
+        company: 'ABC Corp'
+    },
+    {
+        id: 22,
+        site: 'Site V',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 23,
+        site: 'Site W',
+        company: 'ABC Corp'
+    },
+    {
+        id: 24,
+        site: 'Site X',
+        company: 'XYZ Inc'
+    },
+    {
+        id: 25,
+        site: 'Site Y',
+        company: 'ABC Corp'
+    },
+]
+
+export default siteData
