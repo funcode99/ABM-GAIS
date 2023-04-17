fe0109195d0e972bb6d32cc140dde834a25296d5
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-17 16:32:33 +0700

    Added Sort & Search on Page Car

--- src/views/reference/brand/ListBrand.vue
+++ src/views/reference/brand/ListBrand.vue
@@ -177,8 +177,8 @@ const filteredItems = (search) => {
                   </svg>
                 </span>
                 <input
-                  class="placeholder:text-slate-400 placeholder:font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                  placeholder="Search..."
+                  class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-xs capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                  placeholder="Search by Brand / Company"
                   type="text"
                   name="search"
                   v-model="search"
