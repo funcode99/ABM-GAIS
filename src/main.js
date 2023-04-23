6fb5fee09ae2dd2513805abd9f591126ceaf1a1c
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-23 20:57:05 +0700

    Added Global lmport Vue Awesome Paginate

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+import VueAwesomePaginate from 'vue-awesome-paginate'
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(VueAwesomePaginate)
 app.mount('#app')
