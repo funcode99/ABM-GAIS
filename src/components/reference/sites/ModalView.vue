361a012a08ce2886073dd0c2c585d639002975b2
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-28 23:29:04 +0700

    Update on Page Site

--- src/components/reference/sites/ModalView.vue
+++ src/components/reference/sites/ModalView.vue
@@ -1,19 +1,46 @@
 <script setup>
 import iconClose from "@/assets/navbar/icon_close.svg";
-import editicon from "@/assets/navbar/edit_icon.svg";
+import iconview from "@/assets/view_icon.svg";
+
+//code for tags
+// import { ref } from "vue";
+
+// const tags = ref([]);
+
+// function addTag(event) {
+//   if (event.code === "Comma" || event.code === "Enter") {
+//     event.preventDefault();
+//     var val = event.target.value.trim();
+
+//     if (val.length > 0) {
+//       tags.value.push(val);
+//       event.target.value = "";
+//     }
+//   }
+// }
+
+// function removeTag(index) {
+//   tags.value.splice(index, 1);
+// }
+
+// function removeLastTag(event) {
+//   if (event.target.value.length === 0) {
+//     removeTag(tags.value.length - 1);
+//   }
+// }
 </script>
 
 <template>
-  <label for="modal-edit-site" class="cursor-pointer"
-    ><img :src="editicon" class="w-6 h-6"
+  <label for="modal-view-site" class="cursor-pointer"
+    ><img :src="iconview" class="w-6 h-6"
   /></label>
 
-  <input type="checkbox" id="modal-edit-site" class="modal-toggle" />
+  <input type="checkbox" id="modal-view-site" class="modal-toggle" />
   <div class="modal">
     <div class="modal-box relative">
       <nav class="sticky top-0 z-50 bg-[#015289]">
         <label
-          for="modal-edit-site"
+          for="modal-view-site"
           class="cursor-pointer absolute right-3 top-3"
         >
           <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
@@ -21,7 +48,7 @@ import editicon from "@/assets/navbar/edit_icon.svg";
         <p
           class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2 text-start"
         >
-          Edit Site
+          Site
         </p>
       </nav>
 
@@ -36,6 +63,7 @@ import editicon from "@/assets/navbar/edit_icon.svg";
             <select
               class="bg-white w-[320px] lg:w-56 border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
               required
+              disabled
             >
               <option disabled selected>Company</option>
               <option>Company A</option>
@@ -54,7 +82,7 @@ import editicon from "@/assets/navbar/edit_icon.svg";
               name="sitecode"
               class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               placeholder="Site Code"
-              required
+              disabled
             />
           </div>
 
@@ -69,26 +97,43 @@ import editicon from "@/assets/navbar/edit_icon.svg";
               name="site"
               class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               placeholder="Site Name"
-              required
+              disabled
             />
+          </div>
+
+          <div class="mb-6 mr-6 text-start">
+            <label
+              for="warehouse"
+              class="block mb-2 font-JakartaSans font-medium text-sm"
+              >Warehouse<span class="text-red">*</span></label
+            >
+            <div
+              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md text-sm px-4 font-medium sm:text-sm"
+            >
+              <!-- <div
+                v-for="(tag, index) in tags"
+                :key="tag"
+                class="tag-input__tag"
+              >
+                {{ tag }}
+                <span
+                  @click="removeTag(index)"
+                  class="text-xs items-center cursor-pointer"
+                  >x</span
+                >
+              </div> -->
+              <input
+                type="text"
+                placeholder="Enter a site"
+                class="tag-input__text"
+                disabled
+              />
+              <!-- @keydown="addTag"
+                @keydown.delete="removeLastTag" -->
+            </div>
           </div>
         </form>
       </main>
-
-      <div class="sticky bottom-0 bg-white py-2">
-        <div class="flex justify-end gap-4 mr-6">
-          <label
-            for="modal-edit-site"
-            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
-            >Cancel</label
-          >
-          <button
-            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
-          >
-            Save
-          </button>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -109,5 +154,24 @@ import editicon from "@/assets/navbar/edit_icon.svg";
   overflow-y: auto;
   overflow-x: hidden;
   overscroll-behavior-y: contain;
+}
+
+/* .tag-input__tag {
+  height: 30px;
+  float: left;
+  margin-right: 10px;
+  background-color: #e4e4e4;
+  margin-top: 10px;
+  line-height: 30px;
+  padding: 0 5px;
+  border-radius: 5px;
+} */
+
+.tag-input__text {
+  border: none;
+  outline: none;
+  font-size: 0.9em;
+  line-height: 50px;
+  background: none;
 }
 </style>
