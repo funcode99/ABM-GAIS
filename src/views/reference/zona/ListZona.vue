a66261e29ee03d81bed4c0caa70c7beae1f7241b
radenmasabdul <abdul.tamsis@gmail.com>
2023-04-28 22:48:28 +0700

    Update on Page Zona

--- src/views/reference/zona/ListZona.vue
+++ src/views/reference/zona/ListZona.vue
@@ -4,8 +4,6 @@ import Sidebar from "@/components/layout/Sidebar.vue";
 import Footer from "@/components/layout/Footer.vue";
 import ModalAdd from "@/components/reference/zona/ModalAdd.vue";
 import ModalEdit from "@/components/reference/zona/ModalEdit.vue";
-import icon_filter from "@/assets/icon_filter.svg";
-import icon_reset from "@/assets/icon_reset.svg";
 import icon_receive from "@/assets/icon-receive.svg";
 import deleteicon from "@/assets/navbar/delete_icon.svg";
 import arrowicon from "@/assets/navbar/icon_arrow.svg";
@@ -33,23 +31,6 @@ const onChangePage = (pageOfItem) => {
   showingValue.value = pageOfItem;
 };
 
-//for filter & reset button
-const filterDataByCompany = () => {
-  if (selectedCompany.value === "") {
-    sortedData.value = instanceArray;
-  } else {
-    sortedData.value = instanceArray.filter(
-      (item) => item.company === selectedCompany.value
-    );
-  }
-};
-
-//for filter & reset button
-const resetData = () => {
-  sortedData.value = instanceArray;
-  selectedCompany.value = "Company";
-};
-
 //for check & uncheck all
 const selectAll = (checkValue) => {
   const checkList = checkValue;
@@ -69,8 +50,8 @@ const selectAll = (checkValue) => {
 //for tablehead
 const tableHead = [
   { Id: 1, title: "No", jsonData: "no" },
-  { Id: 2, title: "TLK", jsonData: "tlk" },
-  { Id: 3, title: "Company", jsonData: "company" },
+  { Id: 2, title: "Zona", jsonData: "zona" },
+  { Id: 3, title: "City", jsonData: "city" },
   { Id: 5, title: "Actions" },
 ];
 
@@ -94,11 +75,11 @@ onBeforeMount(() => {
 const filteredItems = (search) => {
   sortedData.value = instanceArray;
   const filteredR = sortedData.value.filter((item) => {
-    (item.tlk.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    (item.zona.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.city.toLowerCase().indexOf(search.toLowerCase()) > -1);
     return (
-      (item.tlk.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1)
+      (item.zona.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.city.toLowerCase().indexOf(search.toLowerCase()) > -1)
     );
   });
   sortedData.value = filteredR;
@@ -124,7 +105,7 @@ const filteredItems = (search) => {
             <p
               class="font-JakartaSans text-[10px] lg:text-base md:text-base capitalize text-[#0A0A0A] font-semibold"
             >
-              Zona / Tunjangan Lokasi Kerja
+              Zona
             </p>
 
             <div class="flex gap-4">
@@ -137,48 +118,23 @@ const filteredItems = (search) => {
             </div>
           </div>
 
-          <!-- SORT & SEARCH -->
-          <div class="flex flex-wrap justify-between items-center mx-4 py-2">
-            <div class="flex flex-wrap items-center gap-4">
-              <p
-                class="capitalize font-JakartaSans text-xs text-black font-medium"
-              >
-                Company
-              </p>
-
+          <!-- SEARCH & SHOWING -->
+          <div class="flex flex-wrap justify-between items-center mx-4">
+            <div class="flex items-center gap-1 pt-6 pb-4 h-4">
+              <h1 class="text-xs font-JakartaSans font-normal">Showing</h1>
               <select
-                class="font-JakartaSans bg-white w-full lg:w-40 border border-slate-300 rounded-md py-2 px-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
-                v-model="selectedCompany"
+                class="font-JakartaSans bg-white w-full lg:w-16 border border-slate-300 rounded-md py-1 px-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
+                v-model="pageMultiplier"
               >
-                <option disabled selected>Company</option>
-                <option v-for="data in sortedData" :key="data.id">
-                  {{ data.company }}
-                </option>
+                <option>10</option>
+                <option>25</option>
+                <option>50</option>
+                <option>75</option>
+                <option>100</option>
               </select>
-
-              <div class="flex flex-wrap gap-4 items-center">
-                <button
-                  class="btn btn-sm text-white text-sm font-JakartaSans font-bold capitalize w-[114px] h-[36px] border-green bg-green gap-2 items-center hover:bg-[#099250] hover:text-white hover:border-[#099250]"
-                  @click="filterDataByCompany"
-                >
-                  <span>
-                    <img :src="icon_filter" class="w-5 h-5" />
-                  </span>
-                  Filter
-                </button>
-                <button
-                  class="btn btn-sm text-white text-sm font-JakartaSans font-bold capitalize w-[114px] h-[36px] border-red bg-red gap-2 items-center hover:bg-[#D92D20] hover:text-white hover:border-[#D92D20]"
-                  @click="resetData"
-                >
-                  <span>
-                    <img :src="icon_reset" class="w-5 h-5" />
-                  </span>
-                  Reset
-                </button>
-              </div>
             </div>
 
-            <div class="py-2 flex md:mx-0">
+            <div class="flex md:mx-0">
               <label class="relative block">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                   <svg
@@ -198,8 +154,8 @@ const filteredItems = (search) => {
                   </svg>
                 </span>
                 <input
-                  class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-xs capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                  placeholder="Search bt TLK / Company"
+                  class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-[11px] capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                  placeholder="Search by Zona / City"
                   type="text"
                   name="search"
                   v-model="search"
@@ -207,21 +163,6 @@ const filteredItems = (search) => {
                 />
               </label>
             </div>
-          </div>
-
-          <!-- SHOWING -->
-          <div class="flex items-center gap-1 pt-2 pb-4 px-4 h-4">
-            <h1 class="text-xs font-JakartaSans font-normal">Showing</h1>
-            <select
-              class="font-JakartaSans bg-white w-full lg:w-16 border border-slate-300 rounded-md py-1 px-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
-              v-model="pageMultiplier"
-            >
-              <option>10</option>
-              <option>25</option>
-              <option>50</option>
-              <option>75</option>
-              <option>100</option>
-            </select>
           </div>
 
           <!-- TABLE -->
@@ -275,8 +216,8 @@ const filteredItems = (search) => {
                       <input type="checkbox" name="checks" />
                     </td>
                     <td>{{ data.no }}</td>
-                    <td>{{ data.tlk }}</td>
-                    <td>{{ data.company }}</td>
+                    <td>{{ data.zona }}</td>
+                    <td>{{ data.city }}</td>
                     <td class="flex flex-wrap gap-4 justify-center">
                       <ModalEdit />
                       <button>
