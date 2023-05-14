a372e047168a083a43921448f7c29dd6cfa9ca50
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-14 23:28:52 +0700

    CRUD & Function In UOM

--- src/views/reference/uom/ListUom.vue
+++ src/views/reference/uom/ListUom.vue
@@ -9,6 +9,10 @@ import icon_receive from "@/assets/icon-receive.svg";
 import deleteicon from "@/assets/navbar/delete_icon.svg";
 import arrowicon from "@/assets/navbar/icon_arrow.svg";
 
+import Swal from "sweetalert2";
+
+import Api from "@/utils/Api";
+
 import uomdata from "@/utils/Api/reference/uomdata.js";
 
 import { ref, onBeforeMount, computed } from "vue";
@@ -72,7 +76,7 @@ const sortList = (sortBy) => {
 
 onBeforeMount(() => {
   getSessionForSidebar();
-  instanceArray = uomdata;
+  fetchUom();
   sortedData.value = instanceArray;
   lengthCounter = sortedData.value.length;
 });
@@ -81,31 +85,76 @@ onBeforeMount(() => {
 const filteredItems = (search) => {
   sortedData.value = instanceArray;
   const filteredR = sortedData.value.filter((item) => {
-    (item.uom.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.uom.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    (item.uom_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.uom_name.toLowerCase().indexOf(search.toLowerCase()) > -1);
     return (
-      (item.uom.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.uom.toLowerCase().indexOf(search.toLowerCase()) > -1)
+      (item.uom_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
+      (item.uom_name.toLowerCase().indexOf(search.toLowerCase()) > -1)
     );
   });
   sortedData.value = filteredR;
   lengthCounter = sortedData.value.length;
-  onChangePage(1)
+  onChangePage(1);
 };
 
 const getSessionForSidebar = () => {
   sidebar.setSidebarRefresh(sessionStorage.getItem("isOpen"));
 };
+
+//get all uom
+const fetchUom = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/uom/");
+  instanceArray = res.data.data;
+  sortedData.value = instanceArray;
+  lengthCounter = sortedData.value.length;
+};
+
+const deleteUom = async (id) => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Api.delete(`/uom/delete_data/${id}`).then((res) => {
+        Swal.fire({
+          title: "Successfully",
+          text: "Uom has been deleted.",
+          icon: "success",
+          showCancelButton: false,
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
+        fetchUom();
+      });
+    } else {
+      return;
+    }
+  });
+};
+
+const props = defineProps({
+  id: Number,
+});
 </script>
 
 <template>
   <div
-    class="flex flex-col w-full this"
+    class="flex flex-col w-full this h-[100vh]"
     :class="lockScrollbar === true ? 'fixed' : ''"
   >
     <Navbar />
-    <div class="flex w-screen mt-[115px]">
-      <Sidebar class="flex-none fixed" />
+    <div class="flex w-screen content mt-[115px]">
+      <Sidebar class="flex-none" />
 
       <div
         class="bg-[#e4e4e6] pt-5 pb-16 px-8 w-screen h-full clean-margin ease-in-out duration-500"
@@ -126,7 +175,10 @@ const getSessionForSidebar = () => {
             </p>
 
             <div class="flex gap-4">
-              <ModalAdd @unlock-scrollbar="lockScrollbar = !lockScrollbar"/>
+              <ModalAdd
+                @unlock-scrollbar="lockScrollbar = !lockScrollbar"
+                @uom-saved="fetchUom"
+              />
               <button
                 class="btn btn-md border-green bg-white gap-2 items-center hover:bg-white hover:border-green"
               >
@@ -223,7 +275,7 @@ const getSessionForSidebar = () => {
                 <tbody>
                   <tr
                     class="font-JakartaSans font-normal text-sm"
-                    v-for="data in sortedData.slice(
+                    v-for="(data, index) in sortedData.slice(
                       paginateIndex * pageMultiplierReactive,
                       (paginateIndex + 1) * pageMultiplierReactive
                     )"
@@ -232,11 +284,16 @@ const getSessionForSidebar = () => {
                     <td>
                       <input type="checkbox" name="checks" />
                     </td>
-                    <td>{{ data.no }}</td>
-                    <td>{{ data.uom }}</td>
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ data.uom_name }}</td>
                     <td class="flex flex-wrap gap-4 justify-center">
-                      <ModalEdit @unlock-scrollbar="lockScrollbar = !lockScrollbar"/>
-                      <button>
+                      <ModalEdit
+                        @unlock-scrollbar="lockScrollbar = !lockScrollbar"
+                        :id="data.id"
+                        :key="`edit-uom-${data.id}`"
+                        @uom-update="fetchUom"
+                      />
+                      <button @click="deleteUom(data.id)">
                         <img :src="deleteicon" class="w-6 h-6" />
                       </button>
                     </td>
@@ -267,7 +324,7 @@ const getSessionForSidebar = () => {
           </div>
         </div>
       </div>
-      <Footer class="fixed bottom-0 left-0 right-0" />
+      <Footer />
     </div>
   </div>
 </template>
