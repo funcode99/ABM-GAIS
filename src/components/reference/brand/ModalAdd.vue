0f23df0d9eceec4fa1a54d7dcb218c3e4c939b85
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-16 01:54:18 +0700

    Create, Read, Delete in Brand

--- src/components/reference/brand/ModalAdd.vue
+++ src/components/reference/brand/ModalAdd.vue
@@ -1,7 +1,73 @@
 <script setup>
 import iconClose from "@/assets/navbar/icon_close.svg";
 
-const emits = defineEmits(["unlockScrollbar"]);
+import Swal from "sweetalert2";
+import Api from "@/utils/Api";
+
+import { ref, onMounted } from "vue";
+
+let selectedCompany = ref("Company");
+let selectedSite = ref("Site");
+let brandName = ref("");
+let Company = ref("");
+let Site = ref("");
+let isOpenModal = ref(false);
+
+const emits = defineEmits(["unlockScrollbar", "brand-saved"]);
+
+//for get company in select
+const fetchGetCompany = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/company/get");
+  Company.value = res.data.data;
+  // console.log("ini data parent" + JSON.stringify(res.data.data));
+};
+
+//for get site in select
+const fetchGetSite = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/site/");
+  Site.value = res.data.data;
+  // console.log("ini data parent" + JSON.stringify(res.data.data));
+};
+
+onMounted(() => {
+  fetchGetCompany();
+  fetchGetSite();
+});
+
+const saveBrand = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  try {
+    const payload = {
+      brand_name: brandName.value,
+      id_company: selectedCompany.value,
+      id_site: selectedSite.value,
+    };
+
+    await Api.post(`/brand/store`, payload);
+
+    // Reset nilai input
+    (brandName.value = ""), (selectedCompany.value = "");
+    selectedSite.value = "";
+
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    emits("brand-saved");
+    isOpenModal.value = !isOpenModal.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <template>
@@ -12,7 +78,12 @@ const emits = defineEmits(["unlockScrollbar"]);
     >+ Add New</label
   >
 
-  <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+  <input
+    type="checkbox"
+    id="my-modal-3"
+    class="modal-toggle"
+    v-model="isOpenModal"
+  />
   <div class="modal">
     <div class="modal-box relative">
       <nav class="sticky top-0 z-50 bg-[#015289]">
@@ -29,7 +100,7 @@ const emits = defineEmits(["unlockScrollbar"]);
       </nav>
 
       <main class="modal-box-inner-brand">
-        <form>
+        <form @submit.prevent="saveBrand">
           <div class="mb-6 w-full px-4">
             <label
               for="company"
@@ -39,10 +110,12 @@ const emits = defineEmits(["unlockScrollbar"]);
             <select
               class="cursor-pointer font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               required
+              v-model="selectedCompany"
             >
               <option disabled selected>Company</option>
-              <option>Company A</option>
-              <option>Company B</option>
+              <option v-for="company in Company" :value="company.id">
+                {{ company.company_name }}
+              </option>
             </select>
           </div>
 
@@ -55,10 +128,12 @@ const emits = defineEmits(["unlockScrollbar"]);
             <select
               class="cursor-pointer font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               required
+              v-model="selectedSite"
             >
               <option disabled selected>Site</option>
-              <option>Site A</option>
-              <option>Site B</option>
+              <option v-for="site in Site" :value="site.id">
+                {{ site.site_name }}
+              </option>
             </select>
           </div>
 
@@ -74,6 +149,7 @@ const emits = defineEmits(["unlockScrollbar"]);
               class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               placeholder="Brand Name"
               required
+              v-model="brandName"
             />
           </div>
 
@@ -86,6 +162,7 @@ const emits = defineEmits(["unlockScrollbar"]);
                 >Cancel</label
               >
               <button
+                type="submit"
                 class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
               >
                 Save
--- src/views/reference/brand/ListBrand.vue
+++ src/views/reference/brand/ListBrand.vue
@@ -11,9 +11,13 @@ import icon_receive from "@/assets/icon-receive.svg";
 import deleteicon from "@/assets/navbar/delete_icon.svg";
 import arrowicon from "@/assets/navbar/icon_arrow.svg";
 
+import Swal from "sweetalert2";
+
+import Api from "@/utils/Api";
+
 import brandData from "@/utils/Api/reference/branddata.js";
 
-import { ref, onBeforeMount, computed } from "vue";
+import { ref, onBeforeMount, onMounted, computed } from "vue";
 
 import { useSidebarStore } from "@/stores/sidebar.js";
 const sidebar = useSidebarStore();
@@ -25,6 +29,7 @@ const selectedCompany = ref("Company");
 let sortedbyASC = true;
 let instanceArray = [];
 let lockScrollbar = ref(false);
+let Company = ref("");
 
 //for paginations & showing
 let showingValue = ref(1);
@@ -41,11 +46,11 @@ const onChangePage = (pageOfItem) => {
 
 //for filter & reset button
 const filterDataByCompany = () => {
-  if (selectedCompany.value === "") {
+  if (selectedCompany.value === "Company") {
     sortedData.value = instanceArray;
   } else {
     sortedData.value = instanceArray.filter(
-      (item) => item.company === selectedCompany.value
+      (item) => item.id_company === selectedCompany.value
     );
   }
 };
@@ -93,7 +98,7 @@ const sortList = (sortBy) => {
 
 onBeforeMount(() => {
   getSessionForSidebar();
-  instanceArray = brandData;
+  fetchBrand();
   sortedData.value = instanceArray;
   lengthCounter = sortedData.value.length;
 });
@@ -103,10 +108,10 @@ const filteredItems = (search) => {
   sortedData.value = instanceArray;
   const filteredR = sortedData.value.filter((item) => {
     (item.brand_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1);
+      (item.company_name.toLowerCase().indexOf(search.toLowerCase()) > -1);
     return (
       (item.brand_name.toLowerCase().indexOf(search.toLowerCase()) > -1) |
-      (item.company.toLowerCase().indexOf(search.toLowerCase()) > -1)
+      (item.company_name.toLowerCase().indexOf(search.toLowerCase()) > -1)
     );
   });
   sortedData.value = filteredR;
@@ -117,17 +122,73 @@ const filteredItems = (search) => {
 const getSessionForSidebar = () => {
   sidebar.setSidebarRefresh(sessionStorage.getItem("isOpen"));
 };
+
+//for get company in select
+const fetchGetCompany = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/company/get");
+  Company.value = res.data.data;
+  // console.log("ini data parent" + JSON.stringify(res.data.data));
+};
+
+onMounted(() => {
+  fetchGetCompany();
+});
+
+//get call brand
+const fetchBrand = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/brand/");
+  // console.log(res.data.data);
+  instanceArray = res.data.data;
+  sortedData.value = instanceArray;
+  lengthCounter = sortedData.value.length;
+};
+
+//delete brand
+const deleteBrand = async (id) => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Api.delete(`/brand/delete_data/${id}`).then((res) => {
+        Swal.fire({
+          title: "Successfully",
+          text: "Brand has been deleted.",
+          icon: "success",
+          showCancelButton: false,
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
+        fetchBrand();
+      });
+    } else {
+      return;
+    }
+  });
+};
 </script>
 
 <template>
   <div
-    class="flex flex-col w-full this"
+    class="flex flex-col w-full this h-[100vh]"
     :class="lockScrollbar === true ? 'fixed' : ''"
   >
     <Navbar />
 
-    <div class="flex w-screen mt-[115px]">
-      <Sidebar class="flex-none fixed" />
+    <div class="flex w-screen content mt-[115px]">
+      <Sidebar class="flex-none" />
 
       <div
         class="bg-[#e4e4e6] pt-5 pb-16 px-8 w-screen h-full clean-margin ease-in-out duration-500"
@@ -147,7 +208,10 @@ const getSessionForSidebar = () => {
               Brand
             </p>
             <div class="flex gap-4">
-              <ModalAdd @unlock-scrollbar="lockScrollbar = !lockScrollbar" />
+              <ModalAdd
+                @unlock-scrollbar="lockScrollbar = !lockScrollbar"
+                @brand-saved="fetchBrand"
+              />
               <button
                 class="btn btn-md border-green bg-white gap-2 items-center hover:bg-white hover:border-green"
               >
@@ -173,8 +237,8 @@ const getSessionForSidebar = () => {
                   v-model="selectedCompany"
                 >
                   <option disabled selected>Company</option>
-                  <option v-for="data in sortedData" :key="data.id">
-                    {{ data.company }}
+                  <option v-for="company in Company" :value="company.id">
+                    {{ company.company_name }}
                   </option>
                 </select>
               </div>
@@ -253,6 +317,7 @@ const getSessionForSidebar = () => {
           >
             <div class="block overflow-x-auto">
               <table
+                v-if="sortedData.length > 0"
                 class="table table-zebra table-compact border w-screen sm:w-full h-full rounded-lg"
               >
                 <thead
@@ -288,7 +353,7 @@ const getSessionForSidebar = () => {
                 <tbody>
                   <tr
                     class="font-JakartaSans font-normal text-sm"
-                    v-for="data in sortedData.slice(
+                    v-for="(data, index) in sortedData.slice(
                       paginateIndex * pageMultiplierReactive,
                       (paginateIndex + 1) * pageMultiplierReactive
                     )"
@@ -297,20 +362,29 @@ const getSessionForSidebar = () => {
                     <td>
                       <input type="checkbox" name="checks" />
                     </td>
-                    <td>{{ data.no }}</td>
+                    <td>{{ index + 1 }}</td>
                     <td>{{ data.brand_name }}</td>
-                    <td>{{ data.company }}</td>
+                    <td>{{ data.company_name }}</td>
                     <td class="flex flex-wrap gap-4 justify-center">
                       <ModalEdit
                         @unlock-scrollbar="lockScrollbar = !lockScrollbar"
                       />
-                      <button>
+                      <button @click="deleteBrand(data.id)">
                         <img :src="deleteicon" class="w-6 h-6" />
                       </button>
                     </td>
                   </tr>
                 </tbody>
               </table>
+
+              <div
+                v-else
+                class="h-[100px] border-t border-t-black flex items-center justify-center"
+              >
+                <h1 class="text-center font-JakartaSans text-base font-medium">
+                  Tidak Ada Data
+                </h1>
+              </div>
             </div>
           </div>
 
