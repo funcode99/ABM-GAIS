fdc7584cd4d638a2154ae1f3355059414cfe6fcf
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-19 06:45:09 +0700

    custom modal edit flight

--- src/components/reference/flight/ModalEdit.vue
+++ src/components/reference/flight/ModalEdit.vue
@@ -1,118 +1,101 @@
 <script setup>
 import iconClose from "@/assets/navbar/icon_close.svg";
-import editicon from "@/assets/navbar/edit_icon.svg";
+import editIcon from "@/assets/navbar/edit_icon.svg";
 
 import { ref } from "vue";
+import { Modal } from "usemodal-vue3";
 
 import { useFormEditStore } from "@/stores/reference/flight/edit-modal.js";
 let formEditState = useFormEditStore();
 
+const emits = defineEmits([
+  "unlockScrollbar",
+  "flight-class-update",
+  "changeFlight",
+]);
 let isVisible = ref(false);
-
-const emits = defineEmits(["unlockScrollbar", "changeFlight"]);
+let modalPaddingHeight = 200;
 
 const props = defineProps({
   formContent: Array,
 });
 
-const currentflightClassName = ref(props.formContent[0]);
+const currentFlightClassName = ref(props.formContent[0]);
 
 const submitEdit = () => {
-  formEditState.flight.flightClassName = currentflightClassName.value;
+  formEditState.flight.flightClassName = currentFlightClassName.value;
   isVisible.value = !isVisible.value;
 };
+
+const inputStylingClass =
+  "font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm";
 </script>
 
 <template>
-  <label
-    @click="this.$emit('unlockScrollbar')"
-    for="modal-edit-flight"
-    class="cursor-pointer"
-    ><img :src="editicon" class="w-6 h-6"
-  /></label>
+  <button @click="isVisible = !isVisible">
+    <img :src="editIcon" alt="" />
+  </button>
 
-  <input
-    type="checkbox"
-    id="modal-edit-flight"
-    class="modal-toggle"
-    v-model="isVisible"
-  />
-  <div class="modal">
-    <div class="modal-box relative">
-      <nav class="sticky top-0 z-50 bg-[#015289]">
-        <label
-          @click="this.$emit('unlockScrollbar')"
-          for="modal-edit-flight"
+  <Modal v-model:visible="isVisible" v-model:offsetTop="modalPaddingHeight">
+    <main>
+      <div class="sticky top-0 z-50 bg-[#015289]">
+        <button
+          @click="isVisible = false"
           class="cursor-pointer absolute right-3 top-0 lg:top-3"
         >
           <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
-        </label>
+        </button>
         <p
           class="font-JakartaSans text-sm lg:text-2xl font-semibold text-white mx-4 py-2 text-start"
         >
-          Edit Flight Class
+          Edit Flight
         </p>
-      </nav>
+      </div>
 
-      <main class="modal-box-inner-flight">
-        <div class="pt-4">
-          <div class="mb-6 text-start px-4 w-full">
-            <label
-              for="flight"
-              class="block mb-2 font-JakartaSans font-medium text-sm"
-              >Flight Class<span class="text-red">*</span></label
-            >
-            <input
-              type="text"
-              name="flight"
-              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Flight Class"
-              required
-              v-model="currentflightClassName"
-              @keydown.enter="submitEdit"
-              @keyup.enter="$emit('changeflightClassName')"
-            />
-          </div>
-
-          <div class="sticky bottom-0 bg-white">
-            <div class="flex justify-end gap-4 mr-6">
-              <label
-                @click="this.$emit('unlockScrollbar')"
-                for="modal-edit-flight"
-                class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
-                >Cancel</label
-              >
-              <button @click="submitEdit">
-                <button
-                  @click="$emit('changeFlight')"
-                  class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
-                >
-                  Save
-                </button>
-              </button>
-            </div>
-          </div>
+      <div class="pt-4">
+        <div class="mb-6 text-start px-4 w-full">
+          <label
+            for="flight"
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            >Flight Class<span class="text-red">*</span></label
+          >
+          <input
+            @keydown.enter="submitEdit"
+            @keyup.enter="$emit('changeFlight')"
+            v-model="currentFlightClassName"
+            type="text"
+            id="name"
+            :class="inputStylingClass"
+            required
+          />
         </div>
-      </main>
-    </div>
-  </div>
+      </div>
+
+      <div class="sticky bottom-0 bg-white">
+        <div class="flex justify-end gap-4 mr-4">
+          <label
+            @click="isVisible = !isVisible"
+            for="add-user-modal"
+            class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
+          >
+            Cancel
+          </label>
+          <button @click="submitEdit">
+            <button
+              @click="$emit('changeFlight')"
+              class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
+            >
+              Save
+            </button>
+          </button>
+        </div>
+      </div>
+    </main>
+  </Modal>
 </template>
 
 <style scoped>
-.modal-box {
-  padding: 0;
-  overflow-y: hidden;
-  overscroll-behavior: contain;
-}
-
-.modal-box-inner-flight {
-  --tw-scale-x: 1;
-  --tw-scale-y: 0.9;
-  transform: translate(var(--tw-translate-x), var(--tw-translate-y))
-    rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
-    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
-  overflow-y: auto;
-  overflow-x: hidden;
-  overscroll-behavior-y: contain;
+:deep(.modal-vue3-content) {
+  max-height: 210px !important;
 }
 </style>
