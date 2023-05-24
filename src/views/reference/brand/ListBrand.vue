6160104961ca2ca525a40b95a304891e9a2142ef
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-24 14:00:57 +0700

    Update function in brand

--- src/views/reference/brand/ListBrand.vue
+++ src/views/reference/brand/ListBrand.vue
@@ -10,15 +10,20 @@ import icon_reset from "@/assets/icon_reset.svg";
 import icon_receive from "@/assets/icon-receive.svg";
 import deleteicon from "@/assets/navbar/delete_icon.svg";
 import arrowicon from "@/assets/navbar/icon_arrow.svg";
+import icondanger from "@/assets/Danger.png";
+import iconClose from "@/assets/navbar/icon_close.svg";
 
 import Swal from "sweetalert2";
 
 import Api from "@/utils/Api";
 
-import { useFormEditStore } from "@/stores/reference/brand/edit-modal.js";
+import { Workbook } from "exceljs";
+
 import { ref, onBeforeMount, onMounted, computed } from "vue";
 
+import { useFormEditStore } from "@/stores/reference/brand/edit-modal.js";
 import { useSidebarStore } from "@/stores/sidebar.js";
+
 const sidebar = useSidebarStore();
 const formEditState = useFormEditStore();
 
@@ -31,7 +36,8 @@ let editBrandDataId = ref();
 //for edit
 const editBrand = async (data) => {
   editBrandDataId.value = data;
-  setTimeout(callEditApi, 500);
+  callEditApi();
+  // setTimeout(callEditApi, 500);
   // console.log("ini data id:" + data);
 };
 
@@ -62,13 +68,15 @@ let sortedbyASC = true;
 let instanceArray = [];
 let lockScrollbar = ref(false);
 let Company = ref("");
+let lengthCounter = 0;
+let sortAscending = true;
+let sortedDataReactive = computed(() => sortedData.value);
 
 //for paginations & showing
 let showingValue = ref(1);
 let pageMultiplier = ref(10);
 let pageMultiplierReactive = computed(() => pageMultiplier.value);
 let paginateIndex = ref(0);
-let lengthCounter = 0;
 
 //for paginations
 const onChangePage = (pageOfItem) => {
@@ -111,9 +119,9 @@ const selectAll = (checkValue) => {
 
 //for tablehead
 const tableHead = [
-  { Id: 1, title: "No", jsonData: "no" },
+  { Id: 1, title: "No" },
   { Id: 2, title: "Brand Name", jsonData: "brand_name" },
-  { Id: 3, title: "Company", jsonData: "company" },
+  { Id: 3, title: "Company", jsonData: "company_name" },
   { Id: 4, title: "Actions" },
 ];
 
@@ -168,7 +176,7 @@ onMounted(() => {
   fetchGetCompany();
 });
 
-//get call brand
+//get all brand
 const fetchBrand = async () => {
   const token = JSON.parse(localStorage.getItem("token"));
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -185,13 +193,23 @@ const deleteBrand = async (id) => {
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
 
   Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
+    title:
+      "<span class='font-JakartaSans font-medium text-[28px]'>Are you sure want to delete this?</span>",
+    html: "<div class='font-JakartaSans font-medium text-sm'>This will delete this data permanently, You cannot undo this action.</div>",
+    iconHtml: `<img src="${icondanger}" />`,
+    showCloseButton: true,
+    closeButtonHtml: `<img src="${iconClose}" class="hover:scale-75"/>`,
     showCancelButton: true,
+    buttonsStyling: false,
+    cancelButtonText: "Cancel",
+    customClass: {
+      cancelButton: "swal-cancel-button",
+      confirmButton: "swal-confirm-button",
+    },
+    reverseButtons: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
+    confirmButtonText: "Yes",
   }).then((result) => {
     if (result.isConfirmed) {
       Api.delete(`/brand/delete_data/${id}`).then((res) => {
@@ -200,14 +218,54 @@ const deleteBrand = async (id) => {
           text: "Brand has been deleted.",
           icon: "success",
           showCancelButton: false,
-          confirmButtonColor: "#3085d6",
-          confirmButtonText: "Ok",
+          confirmButtonColor: "#015289",
+          showConfirmButton: false,
+          timer: 1500,
         });
         fetchBrand();
       });
     } else {
       return;
     }
+  });
+};
+
+//for export
+const exportToExcel = () => {
+  const workbook = new Workbook();
+  const worksheet = workbook.addWorksheet("Brand Data");
+
+  const tableHead = [
+    { title: "Nomor" },
+    { title: "ID" },
+    { title: "Brand Name" },
+    { title: "Company" },
+  ];
+
+  // Menambahkan header kolom
+  tableHead.forEach((column, index) => {
+    worksheet.getCell(1, index + 1).value = column.title;
+  });
+
+  // Menambahkan data ke baris-baris selanjutnya
+  sortedDataReactive.value.forEach((data, rowIndex) => {
+    worksheet.getCell(rowIndex + 2, 1).value = rowIndex + 1;
+    worksheet.getCell(rowIndex + 2, 2).value = data.id;
+    worksheet.getCell(rowIndex + 2, 3).value = data.brand_name;
+    worksheet.getCell(rowIndex + 2, 4).value = data.company_name;
+  });
+
+  // Menyimpan workbook menjadi file Excel
+  workbook.xlsx.writeBuffer().then((buffer) => {
+    const blob = new Blob([buffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "brand_data.xlsx";
+    a.click();
+    URL.revokeObjectURL(url);
   });
 };
 </script>
@@ -246,6 +304,7 @@ const deleteBrand = async (id) => {
               />
               <button
                 class="btn btn-md border-green bg-white gap-2 items-center hover:bg-white hover:border-green"
+                @click="exportToExcel"
               >
                 <img :src="icon_receive" class="w-6 h-6" />
               </button>
@@ -374,7 +433,7 @@ const deleteBrand = async (id) => {
                     >
                       <span class="flex justify-center items-center gap-1">
                         {{ data.title }}
-                        <button>
+                        <button v-if="data.jsonData" class="ml-2">
                           <img :src="arrowicon" class="w-[9px] h-3" />
                         </button>
                       </span>
@@ -389,12 +448,15 @@ const deleteBrand = async (id) => {
                       paginateIndex * pageMultiplierReactive,
                       (paginateIndex + 1) * pageMultiplierReactive
                     )"
-                    :key="data.no"
+                    :key="data.id"
                   >
                     <td>
                       <input type="checkbox" name="checks" />
                     </td>
-                    <td>{{ index + 1 }}</td>
+                    <td>
+                      {{ index + 1 + paginateIndex * pageMultiplierReactive }}
+                    </td>
+                    <!-- <td>{{ data.id }}</td> -->
                     <td>{{ data.brand_name }}</td>
                     <td>{{ data.company_name }}</td>
                     <td class="flex flex-wrap gap-4 justify-center">
@@ -402,9 +464,9 @@ const deleteBrand = async (id) => {
                         @unlock-scrollbar="lockScrollbar = !lockScrollbar"
                         @change-brand="editBrand(data.id)"
                         :formContent="[
-                          data.brandName,
-                          data.brandIdCompany,
-                          data.brandIdSite,
+                          data.brand_name,
+                          data.id_company,
+                          data.id_site,
                         ]"
                       />
                       <button @click="deleteBrand(data.id)">
@@ -442,12 +504,12 @@ const deleteBrand = async (id) => {
               v-model="showingValue"
               :max-pages-shown="4"
               :show-breakpoint-buttons="false"
-              :show-jump-buttons="true"
+              :show-ending-buttons="true"
             />
           </div>
         </div>
       </div>
-      <Footer class="fixed bottom-0 left-0 right-0" />
+      <Footer />
     </div>
   </div>
 </template>
