0ecb7facf08b98df08e7e77b21ec0e504247230f
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-27 01:08:35 +0700

    done in brand

--- src/components/reference/brand/ModalEdit.vue
+++ src/components/reference/brand/ModalEdit.vue
@@ -1,6 +1,8 @@
 <script setup>
-import iconClose from "@/assets/navbar/icon_close.svg";
 import editicon from "@/assets/navbar/edit_icon.svg";
+
+import modalHeaderEdit from "@/components/modal/edit/ModalHeaderEdit.vue";
+import ModalFooterEdit from "@/components/modal/edit/ModalFooterEdit.vue";
 
 import Api from "@/utils/Api";
 
@@ -11,7 +13,7 @@ import { useFormEditStore } from "@/stores/reference/brand/edit-modal.js";
 let formEditState = useFormEditStore();
 
 let isVisible = ref(false);
-let modalPaddingHeight = 118;
+let modalPaddingHeight = "19%";
 
 let selectedCompany = ref("Company");
 let Company = ref("");
@@ -78,48 +80,32 @@ const resetForm = () => {
   currentbrandName.value = originalbrandName.value;
   selectedCompanyId.value = originalbrandIdCompany.value;
   selectedSiteId.value = originalbrandIdSite.value;
+
+  isVisible.value = !isVisible.value;
 };
 
 const inputStylingClass =
   "font-JakartaSans block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm";
-
 </script>
 
 <template>
-
-  <button
-    @click="
-      resetForm();
-      isVisible = !isVisible;
-    "
-  >
-    <img :src="editicon" alt="" />
+  <button @click="resetForm()">
+    <img :src="editicon" alt="edit icon" />
   </button>
 
   <Modal v-model:visible="isVisible" v-model:offsetTop="modalPaddingHeight">
-
     <main>
-      
-      <div class="sticky top-0 z-50 bg-[#015289]">
-        <button
-          @click="isVisible = false"
-          class="cursor-pointer absolute right-3 top-0 lg:top-3"
-        >
-          <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
-        </button>
-        <p
-          class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2 text-start"
-        >
-          Edit Brand
-        </p>
-      </div>
+      <modalHeaderEdit
+        @closeVisibility="isVisible = false"
+        title="Edit Brand"
+      />
 
       <div class="pt-4">
-
         <div class="mb-6 text-start w-full px-4">
-          
-          <label class="block mb-2 font-JakartaSans font-medium text-sm"
-            for="company">
+          <label
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            for="company"
+          >
             Company<span class="text-red">*</span>
           </label>
 
@@ -133,17 +119,18 @@ const inputStylingClass =
               {{ company.company_name }}
             </option>
           </select>
-
         </div>
 
         <div class="mb-6 text-start w-full px-4">
-          
-          <label class="block mb-2 font-JakartaSans font-medium text-sm"
-            for="site">
+          <label
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            for="site"
+          >
             Site<span class="text-red">*</span>
           </label>
 
-          <select class="cursor-pointer font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          <select
+            class="cursor-pointer font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             required
             v-model="selectedSiteId"
           >
@@ -152,7 +139,6 @@ const inputStylingClass =
               {{ site.site_name }}
             </option>
           </select>
-
         </div>
 
         <div class="mb-6 w-full px-4">
@@ -171,33 +157,15 @@ const inputStylingClass =
           />
         </div>
 
-        <div class="sticky bottom-0 bg-white">
-          <div class="flex justify-end gap-4 mr-6">
-            <label
-              @click="
-                resetForm();
-                isVisible = !isVisible;
-              "
-              for="modal-edit"
-              class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
-              >Cancel</label
-            >
-            <button
-              @click="
-                submitEdit();
-                $emit('changeBrand');
-              "
-              class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-
+        <ModalFooterEdit
+          @closeEdit="resetForm()"
+          @submitEditForm="
+            submitEdit();
+            $emit('changeBrand');
+          "
+        />
       </div>
-
     </main>
-
   </Modal>
 </template>
 
