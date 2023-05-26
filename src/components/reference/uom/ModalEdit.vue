a66cd4532414542ed99e7385004f1d904f6413ee
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-27 00:21:06 +0700

    done in uom

--- src/components/reference/uom/ModalEdit.vue
+++ src/components/reference/uom/ModalEdit.vue
@@ -1,6 +1,8 @@
 <script setup>
-import iconClose from "@/assets/navbar/icon_close.svg";
 import editicon from "@/assets/navbar/edit_icon.svg";
+
+import modalHeaderEdit from "@/components/modal/edit/ModalHeaderEdit.vue";
+import ModalFooterEdit from "@/components/modal/edit/ModalFooterEdit.vue";
 
 import { ref } from "vue";
 import { Modal } from "usemodal-vue3";
@@ -10,7 +12,7 @@ let formEditState = useFormEditStore();
 
 const emits = defineEmits(["unlockScrollbar", "changeUom"]);
 let isVisible = ref(false);
-let modalPaddingHeight = 207;
+let modalPaddingHeight = "34%";
 
 const props = defineProps({
   formContent: Array,
@@ -32,8 +34,10 @@ const submitEdit = () => {
   isVisible.value = !isVisible.value;
   emits("changeUom"); // Memanggil event 'changeUom'
 };
+
 const resetForm = () => {
   currentUomName.value = originalUomName.value;
+  isVisible.value = !isVisible.value;
 };
 
 const inputStylingClass =
@@ -41,30 +45,13 @@ const inputStylingClass =
 </script>
 
 <template>
-  <button
-    @click="
-      resetForm();
-      isVisible = !isVisible;
-    "
-  >
-    <img :src="editicon" alt="" />
+  <button @click="resetForm()">
+    <img :src="editicon" alt="edit icon" />
   </button>
 
   <Modal v-model:visible="isVisible" v-model:offsetTop="modalPaddingHeight">
     <main>
-      <div class="sticky top-0 z-50 bg-[#015289]">
-        <button
-          @click="isVisible = false"
-          class="cursor-pointer absolute right-3 top-0 lg:top-3"
-        >
-          <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
-        </button>
-        <p
-          class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2 text-start"
-        >
-          Edit UOM
-        </p>
-      </div>
+      <modalHeaderEdit @closeVisibility="isVisible = false" title="Edit UOM" />
 
       <div class="pt-4">
         <div class="mb-6 text-start w-full px-4">
@@ -83,28 +70,13 @@ const inputStylingClass =
           />
         </div>
 
-        <div class="sticky bottom-0 bg-white">
-          <div class="flex justify-end gap-4 mr-6">
-            <label
-              @click="
-                resetForm();
-                isVisible = !isVisible;
-              "
-              for="modal-edit-uom"
-              class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
-              >Cancel</label
-            >
-            <button
-              @click="
-                submitEdit();
-                $emit('changeUom');
-              "
-              class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
-            >
-              Save
-            </button>
-          </div>
-        </div>
+        <ModalFooterEdit
+          @closeEdit="resetForm()"
+          @submitEditForm="
+            submitEdit();
+            $emit('changeUom');
+          "
+        />
       </div>
     </main>
   </Modal>
