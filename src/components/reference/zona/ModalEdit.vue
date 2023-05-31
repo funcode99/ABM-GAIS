42a344294df6df58f94e7af39ff8ec628bb9a52d
radenmasabdul <abdul.tamsis@gmail.com>
2023-05-31 23:32:51 +0700

    done crud in zona

--- src/components/reference/zona/ModalEdit.vue
+++ src/components/reference/zona/ModalEdit.vue
@@ -1,200 +1,252 @@
 <script setup>
-import iconClose from "@/assets/navbar/icon_close.svg";
 import editicon from "@/assets/navbar/edit_icon.svg";
+
+import modalHeader from "@/components/modal/modalHeader.vue";
+import modalFooter from "@/components/modal/modalFooter.vue";
 
 import Api from "@/utils/Api";
 
-import { ref, onMounted, watch } from "vue";
+import Multiselect from "@vueform/multiselect";
 
-const emits = defineEmits(["unlockScrollbar"]);
-let selectedCompany = ref("Company");
-let Company = ref("");
+import { ref, onMounted, watch } from "vue";
+import { Modal } from "usemodal-vue3";
+
+import { useFormEditStore } from "@/stores/reference/zona/edit-modal.js";
+
+const emits = defineEmits(["unlockScrollbar", "changeZona"]);
+
+let formEditState = useFormEditStore();
+let isVisible = ref(false);
+let modalPaddingHeight = "25vh";
+let isAdding = ref(false);
+
+let companyData = ref(null);
+let companyIdArray = ref([]);
+let cityData = ref(null);
+let cityIdArray = ref([]);
+
+let companyIdObject = ref(props.formContent[0]);
+let companyIdObjectKeys = ref(Object.values(companyIdObject.value));
+
+let currentZonaName = ref(props.formContent[1]);
+
+let CityIdObject = ref(props.formContent[2]);
+let cityIdObjectKeys = ref(Object.values(CityIdObject.value));
+
+companyIdObjectKeys.value.map((item) => {
+  let number = Number(item);
+
+  if (Number.isInteger(number)) {
+    companyIdArray.value.push(item);
+  }
+});
+
+cityIdObjectKeys.value.map((item) => {
+  let number = Number(item);
+
+  if (Number.isInteger(number)) {
+    cityIdArray.value.push(item);
+  }
+});
+
+const props = defineProps({
+  formContent: Array,
+});
+
+const submitEdit = () => {
+  isAdding.value = true;
+
+  if (!formEditState.zona) {
+    formEditState.zona = {}; // Inisialisasi objek jika belum ada
+  }
+
+  formEditState.zona.zonaIdCompany = companyIdArray.value;
+  formEditState.zona.zonaName = currentZonaName.value;
+  formEditState.zona.zonaIdCity = cityIdArray.value;
+
+  // console.log("nilai zona name" + JSON.stringify(currentZonaName));
+
+  isVisible.value = false;
+  emits("changeZona"); // Memanggil event 'changeZona'
+};
+
+//for get city in input
+const fetchCity = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/city/");
+  cityData = res.data.data;
+  // console.log("ini data city" + JSON.stringify(res.data.data));
+  cityData.map((item) => {
+    item.value = item.id;
+  });
+  // console.log("Data company setelah perubahan:", cityData);
+};
 
 //for get company in input
 const fetchGetCompany = async () => {
   const token = JSON.parse(localStorage.getItem("token"));
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
   const res = await Api.get("/company/get");
-  Company.value = res.data.data;
-  // console.log("ini data parent" + JSON.stringify(res.data.data));
+  companyData = res.data.data;
+  // console.log("ini data company" + JSON.stringify(res.data.data));
+  companyData.map((item) => {
+    item.value = item.id;
+  });
+  // console.log("Data company setelah perubahan:", companyData);
 };
 
 onMounted(() => {
+  fetchCity();
   fetchGetCompany();
 });
 
-const tags = ref([]);
+const inputStylingClass =
+  "font-JakartaSans block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm";
 
-function addTag(event) {
-  if (event.code === "Comma" || event.code === "Enter") {
-    event.preventDefault();
-    var val = event.target.value.trim();
+const resetInput = () => {
+  currentZonaName.value = props.formContent[1];
+  companyIdArray.value = [...props.formContent[0]];
+  cityIdArray.value = [...props.formContent[2]];
+};
 
-    if (val.length > 0) {
-      tags.value.push(val);
-      event.target.value = "";
-    }
+watch(isVisible, (newValue) => {
+  if (newValue) {
+    resetInput();
   }
-}
-
-function removeTag(index) {
-  tags.value.splice(index, 1);
-}
-
-function removeLastTag(event) {
-  if (event.target.value.length === 0) {
-    removeTag(tags.value.length - 1);
-  }
-}
+});
 </script>
 
 <template>
-  <label
-    @click="this.$emit('unlockScrollbar')"
-    for="modal-edit-zona"
-    class="cursor-pointer"
-    ><img :src="editicon" class="w-6 h-6"
-  /></label>
+  <button @click="isVisible = !isVisible">
+    <img :src="editicon" alt="edit icon" />
+  </button>
 
-  <input type="checkbox" id="modal-edit-zona" class="modal-toggle" />
-  <div class="modal">
-    <div class="modal-box relative">
-      <nav class="sticky top-0 z-50 bg-[#015289]">
-        <label
-          @click="this.$emit('unlockScrollbar')"
-          for="modal-edit-zona"
-          class="cursor-pointer absolute right-3 top-3"
-        >
-          <img :src="iconClose" class="w-[34px] h-[34px] hover:scale-75" />
-        </label>
-        <p
-          class="font-JakartaSans text-2xl font-semibold text-white mx-4 py-2 text-start"
-        >
-          Edit Zona
-        </p>
-      </nav>
+  <Modal v-model:visible="isVisible" v-model:offsetTop="modalPaddingHeight">
+    <main>
+      <modalHeader @closeVisibility="isVisible = false" title="Edit Zona" />
 
-      <main class="modal-box-inner-zona">
-        <form class="pt-4">
-          <div class="mb-6 w-full px-4 text-start">
-            <label
-              for="company"
-              class="block mb-2 font-JakartaSans font-medium text-sm"
-              >Company<span class="text-red">*</span></label
-            >
-            <select
-              class="cursor-pointer font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              required
-              v-model="selectedCompany"
-            >
-              <option disabled selected>Company</option>
-              <option v-for="company in Company" :value="company.id">
-                {{ company.company_name }}
-              </option>
-            </select>
-          </div>
+      <form class="pt-4 modal-box-inner-zona" @submit.prevent="submitEdit">
+        <div class="mb-6 w-full px-4 text-start">
+          <label
+            for="company"
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            >Company<span class="text-red">*</span></label
+          >
+          <div
+            class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md text-sm font-medium sm:text-sm"
+          ></div>
 
-          <div class="mb-6 w-full px-4 text-start">
-            <label
-              for="zona"
-              class="block mb-2 font-JakartaSans font-medium text-sm"
-              >Zona<span class="text-red">*</span></label
-            >
-            <input
-              type="text"
-              name="zona"
-              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Zona"
-              required
-            />
-          </div>
-
-          <div class="mb-6 text-start w-full px-4">
-            <label
-              for="city"
-              class="block mb-2 font-JakartaSans font-medium text-sm"
-              >City<span class="text-red">*</span></label
-            >
-            <div
-              class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md text-sm font-medium sm:text-sm"
-            >
+          <Multiselect
+            id="company"
+            v-model="companyIdArray"
+            mode="tags"
+            placeholder="Select Company"
+            track-by="company_name"
+            label="company_name"
+            :close-on-select="false"
+            :searchable="true"
+            :options="companyData"
+          >
+            <template v-slot:tag="{ option, handleTagRemove, disabled }">
               <div
-                v-for="(tag, index) in tags"
-                :key="tag"
-                class="tag-input__tag"
+                class="multiselect-tag is-user"
+                :class="{
+                  'is-disabled': disabled,
+                }"
               >
-                {{ tag }}
+                {{ option.company_name }}
                 <span
-                  @click="removeTag(index)"
-                  class="text-xs items-center cursor-pointer"
-                  >x</span
+                  v-if="!disabled"
+                  class="multiselect-tag-remove"
+                  @click="handleTagRemove(option, $event)"
                 >
+                  <span class="multiselect-tag-remove-icon"></span>
+                </span>
               </div>
-              <input
-                type="text"
-                placeholder="Enter a city"
-                class="tag-input__text px-4 text-sm font-medium w-full font-JakartaSans focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
-                @keydown="addTag"
-                @keydown.delete="removeLastTag"
-              />
-            </div>
-          </div>
+            </template>
+          </Multiselect>
+        </div>
 
-          <div class="sticky bottom-0 bg-white py-2">
-            <div class="flex justify-end gap-4 mr-6">
-              <label
-                @click="this.$emit('unlockScrollbar')"
-                for="modal-edit-zona"
-                class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] bg-red border-red hover:bg-white hover:border-red hover:text-red"
-                >Cancel</label
+        <div class="mb-6 w-full px-4 text-start">
+          <label
+            for="zona"
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            >Zona<span class="text-red">*</span></label
+          >
+          <input
+            type="text"
+            name="zona"
+            placeholder="Zona"
+            required
+            v-model="currentZonaName"
+            :class="inputStylingClass"
+          />
+        </div>
+
+        <div class="mb-6 w-full px-4 text-start">
+          <label
+            for="city"
+            class="block mb-2 font-JakartaSans font-medium text-sm"
+            >City<span class="text-red">*</span></label
+          >
+          <div
+            class="font-JakartaSans capitalize block bg-white w-full border border-slate-300 rounded-md text-sm font-medium sm:text-sm"
+          ></div>
+
+          <Multiselect
+            id="city"
+            v-model="cityIdArray"
+            mode="tags"
+            placeholder="Select City"
+            track-by="city_name"
+            label="city_name"
+            :close-on-select="false"
+            :searchable="true"
+            :options="cityData"
+          >
+            <template v-slot:tag="{ option, handleTagRemove, disabled }">
+              <div
+                class="multiselect-tag is-user"
+                :class="{
+                  'is-disabled': disabled,
+                }"
               >
-              <button
-                class="btn text-white text-base font-JakartaSans font-bold capitalize w-[141px] border-green bg-green hover:bg-white hover:text-green hover:border-green"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </form>
-      </main>
-    </div>
-  </div>
+                {{ option.city_name }}
+                <span
+                  v-if="!disabled"
+                  class="multiselect-tag-remove"
+                  @click="handleTagRemove(option, $event)"
+                >
+                  <span class="multiselect-tag-remove-icon"></span>
+                </span>
+              </div>
+            </template>
+          </Multiselect>
+        </div>
+
+        <modalFooter @closeEdit="isVisible = false" />
+      </form>
+    </main>
+  </Modal>
 </template>
 
 <style scoped>
-.modal-box {
-  padding: 0;
-  overflow-y: hidden;
-  overscroll-behavior: contain;
+:deep(.modal-vue3-content) {
+  max-height: 400px !important;
+  max-width: 510px !important;
 }
 
 .modal-box-inner-zona {
+  max-height: 340px !important;
   --tw-scale-x: 1;
-  --tw-scale-y: 0.9;
+  --tw-scale-y: 1;
   transform: translate(var(--tw-translate-x), var(--tw-translate-y))
     rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
     scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
   overflow-y: auto;
   overflow-x: hidden;
   overscroll-behavior-y: contain;
-}
-
-.tag-input__tag {
-  height: 30px;
-  float: left;
-  margin-right: 10px;
-  background-color: #e4e4e4;
-  margin-top: 10px;
-  line-height: 30px;
-  padding: 0 5px;
-  border-radius: 5px;
-}
-
-.tag-input__text {
-  border: none;
-  outline: none;
-  font-size: 0.9em;
-  line-height: 50px;
-  background: none;
+  overscroll-behavior-y: contain;
 }
 </style>
