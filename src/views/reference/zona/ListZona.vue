2c6325bcde7a85d21193bca033413bd390d487eb
radenmasabdul <abdul.tamsis@gmail.com>
2023-06-14 14:08:55 +0700

    update in zona

--- src/views/reference/zona/ListZona.vue
+++ src/views/reference/zona/ListZona.vue
@@ -63,17 +63,17 @@ const callEditApi = async () => {
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
   await Api.post(`/zona/update_data/${editZonaDataid.value}`, {
     id_company: formEditState.zona.zonaIdCompany,
-    zona_name: formEditState.zona.zonaName,
+    id_zona: formEditState.zona.zonaName,
     id_city: formEditState.zona.zonaIdCity,
   });
-  Swal.fire({
-    position: "center",
-    icon: "success",
-    title: "Your work has been saved",
-    showConfirmButton: false,
-    timer: 1500,
-  });
-  fetchZona();
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    fetchZona();
 };
 
 //for paginations
@@ -467,7 +467,7 @@ const exportToExcel = () => {
                   <input type="checkbox" name="checks" />
                 </td>
                 <td style="width: 5%">{{ data.no }}</td>
-                <td style="width: 20%">{{ data.zona }}</td>
+                <td style="width: 20%">{{ data.zona_name }}</td>
                 <td style="width: 50%">
                   <span
                     :class="[
@@ -478,10 +478,15 @@ const exportToExcel = () => {
                     {{ data.city_name }}
                   </span>
                 </td>
+
                 <td class="flex flex-wrap gap-4 justify-center">
                   <ModalEdit
                     @change-zona="editZona(data.id)"
-                    :formContent="[data.comp_array, data.zona, data.city_array]"
+                    :formContent="[
+                      data.comp_array,
+                      data.id_zona,
+                      data.city_array,
+                    ]"
                   />
                   <button @click="deleteZona(data.id)">
                     <img :src="deleteicon" class="w-6 h-6" />
