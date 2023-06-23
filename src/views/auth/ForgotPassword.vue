7ebc8f902295c90b95a64af4f76261bea63918fb
Idris <luth.idris.99@gmail.com>
2023-06-23 16:20:59 +0700

    add forgot password api

--- src/views/auth/ForgotPassword.vue
+++ src/views/auth/ForgotPassword.vue
@@ -7,14 +7,17 @@
     import { ref } from 'vue'
 
     let email = ref('')
+    let serverResponse = ref('')
 
     const forgotPassword = async () => {
+        serverResponse.value = 0
         const token = JSON.parse(localStorage.getItem('token'))
         Api.defaults.headers.common.Authorization = `Bearer ${token}`
         const api = await Api.post(`/forget_password`, {
             email: email.value
         })
         console.log(api)
+        serverResponse.value = api.status
     }
     
 </script>
@@ -24,23 +27,41 @@
     <NavbarLogin />
 
     <section class="w-full h-screen font-JakartaSans">
+
         <div class="flex items-center justify-center py-[30px] bg-[#e4e4e6] w-full h-full">
-                <div class="flex flex-wrap items-center justify-center gap-8 bg-white px-[6%] py-[3%] rounded-lg">
-                    <div class="hidden sm:block">
-                        <img :src=forgotPasswordImage class="w-[574px] h-[373px] object-scale-down" alt="">
+
+                <div class="flex flex-col flex-wrap items-center justify-center gap-8 bg-white px-[6%] py-[3%] rounded-lg">
+                    
+                    <div v-if="serverResponse == 0" class="text-bold text-lg text-blue bg-lime-200 py-5 w-full text-center rounded-lg">
+                        Waiting for response...
                     </div>
-                    <div class="flex-1 h-full">
-                        <div class="mx-auto mb-16">
-                            <h1 class="text-3xl font-semibold">Forgot Your Password?</h1>
-                            <h1 class="text-sm font-medium">Please enter the email associated with your account.</h1>
+
+                    <div v-if="serverResponse == 200" class="text-bold text-lg text-red bg-yellow-50 py-5 w-full text-center rounded-lg">
+                        A message has been sent to your email address, check it to get your password
+                    </div>
+
+                    <div class="flex flex-wrap items-center justify-center gap-8">
+                        <div class="hidden sm:block">
+                            <img :src=forgotPasswordImage class="w-[574px] h-[373px] object-scale-down" alt="">
                         </div>
-                        <form class="flex flex-col" @submit.prevent="forgotPassword">
-                            <label for="email" class="text-sm font-medium">Email<span class="text-[#f5333f] text-sm font-semibold">*</span></label>
-                            <input type="text" id="email" v-model="email" class="border-black border-2 px-4 py-3 rounded-lg mt-1" placeholder="Input Your Email">
-                        </form>
+    
+                        <div class="flex-1 h-full">
+                            <div class="mx-auto mb-16">
+                                <h1 class="text-3xl font-semibold">Forgot Your Password?</h1>
+                                <h1 class="text-sm font-medium">Please enter the email associated with your account.</h1>
+                            </div>
+                            <form class="flex flex-col" @submit.prevent="forgotPassword">
+                                <label for="email" class="text-sm font-medium">Email<span class="text-[#f5333f] text-sm font-semibold">*</span></label>
+                                <input type="text" id="email" v-model="email" class="border-black border-2 px-4 py-3 rounded-lg mt-1" placeholder="Input Your Email">
+                            </form>
+                        </div>
                     </div>
+
+
                 </div>
+
         </div>
+
     </section>
 
     <Footer />
