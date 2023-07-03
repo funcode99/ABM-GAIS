513411576c2c53f788b77a3690ce645deb037d54
radenmasabdul <abdul.tamsis@gmail.com>
2023-07-03 11:01:33 +0700

    add skeleton in zona

--- src/views/reference/zona/ListZona.vue
+++ src/views/reference/zona/ListZona.vue
@@ -8,6 +8,7 @@ import ModalEdit from "@/components/reference/zona/ModalEdit.vue";
 import tableContainer from "@/components/table/tableContainer.vue";
 import tableTop from "@/components/table/tableTop.vue";
 import tableData from "@/components/table/tableData.vue";
+import SkeletonLoadingTable from "@/components/layout/SkeletonLoadingTable.vue";
 
 import icon_filter from "@/assets/icon_filter.svg";
 import icon_reset from "@/assets/icon_reset.svg";
@@ -27,6 +28,7 @@ import Api from "@/utils/Api";
 
 import { Workbook } from "exceljs";
 import { ref, onBeforeMount, computed, watch } from "vue";
+
 import { useSidebarStore } from "@/stores/sidebar.js";
 import { useFormEditStore } from "@/stores/reference/zona/edit-modal.js";
 import { useReferenceFetchResult } from "@/stores/fetch/reference";
@@ -35,7 +37,6 @@ const sidebar = useSidebarStore();
 let formEditState = useFormEditStore();
 const referenceFetch = useReferenceFetchResult();
 
-//for sort & search
 let sortedData = ref([]);
 let Company = ref("");
 let addCityData = ref([]);
@@ -49,20 +50,17 @@ let sortedDataReactive = computed(() => sortedData.value);
 const showFullText = ref({});
 let checkList = false;
 
-//for paginations
 let showingValue = ref(1);
 let pageMultiplier = ref(10);
 let pageMultiplierReactive = computed(() => pageMultiplier.value);
 let paginateIndex = ref(0);
 let editZonaDataid = ref();
 
-//for edit
 const editZona = async (data) => {
   editZonaDataid.value = data;
   setTimeout(callEditApi, 500);
 };
 
-//for edit
 const callEditApi = async () => {
   const token = JSON.parse(localStorage.getItem("token"));
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -83,13 +81,11 @@ const callEditApi = async () => {
   fetchZona();
 };
 
-//for paginations
 const onChangePage = (pageOfItem) => {
   paginateIndex.value = pageOfItem - 1;
   showingValue.value = pageOfItem;
 };
 
-//for filter & reset button
 const filterDataByCompany = async () => {
   const token = JSON.parse(localStorage.getItem("token"));
   Api.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -99,13 +95,11 @@ const filterDataByCompany = async () => {
   onChangePage(1);
 };
 
-//for filter & reset button
 const resetData = () => {
   fetchZona();
   selectedCompany.value = "Company";
 };
 
-//for check & uncheck all
 const selectAll = (checkValue) => {
   const check = document.getElementsByName("checks");
   const btnDelete = document.getElementById("btnDelete");
@@ -131,13 +125,11 @@ const deleteDataInCeklis = () => {
   const check = document.getElementsByName("checks");
   for (let i = 0; i < check.length; i++) {
     if (check[i].type === "checkbox" && check[i].checked) {
-      // Lakukan tindakan penghapusan data yang sesuai di sini
       const row = check[i].parentNode.parentNode;
       row.parentNode.removeChild(row);
     }
   }
 
-  // Setelah penghapusan, sembunyikan kembali button hapus jika tidak ada checkbox yang terceklis
   const btnDelete = document.getElementById("btnDelete");
   const checkedCheckboxes = document.querySelectorAll(
     'input[name="checks"]:checked'
@@ -147,7 +139,6 @@ const deleteDataInCeklis = () => {
   }
 };
 
-//for tablehead
 const tableHead = [
   { Id: 1, title: "No", jsonData: "no" },
   { Id: 2, title: "Zona", jsonData: "zona_name" },
@@ -155,7 +146,6 @@ const tableHead = [
   { Id: 5, title: "Actions" },
 ];
 
-//for sort
 const sortList = (sortBy) => {
   if (sortedbyASC) {
     sortedData.value.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
@@ -166,7 +156,6 @@ const sortList = (sortBy) => {
   }
 };
 
-//for searching
 const filteredItems = (search) => {
   sortedData.value = instanceArray;
   const filteredR = sortedData.value.filter((item) => {
@@ -231,7 +220,6 @@ const deleteZona = async (id) => {
   });
 };
 
-//for export
 const exportToExcel = () => {
   const workbook = new Workbook();
   const worksheet = workbook.addWorksheet("Zona Data");
@@ -243,12 +231,10 @@ const exportToExcel = () => {
     { title: "City" },
   ];
 
-  // Menambahkan header kolom
   tableHead.forEach((column, index) => {
     worksheet.getCell(1, index + 1).value = column.title;
   });
 
-  // Menambahkan data ke baris-baris selanjutnya
   sortedDataReactive.value.forEach((data, rowIndex) => {
     worksheet.getCell(rowIndex + 2, 1).value = rowIndex + 1;
     worksheet.getCell(rowIndex + 2, 2).value = data.id;
@@ -256,7 +242,6 @@ const exportToExcel = () => {
     worksheet.getCell(rowIndex + 2, 4).value = data.city_name;
   });
 
-  // Menyimpan workbook menjadi file Excel
   workbook.xlsx.writeBuffer().then((buffer) => {
     const blob = new Blob([buffer], {
       type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
@@ -496,6 +481,42 @@ watch(baitArray, () => {
             </tbody>
           </tableData>
 
+          <tableData
+            v-else-if="sortedData.length == 0 && instanceArray.length == 0"
+          >
+            <thead class="text-center font-JakartaSans text-sm font-bold h-10">
+              <tr>
+                <th>
+                  <div class="flex justify-center">
+                    <input
+                      type="checkbox"
+                      name="checked"
+                      @click="selectAll((checkList = !checkList))"
+                    />
+                  </div>
+                </th>
+
+                <th
+                  v-for="data in tableHead"
+                  :key="data.Id"
+                  class="overflow-x-hidden cursor-pointer"
+                  @click="sortList(`${data.jsonData}`)"
+                >
+                  <div class="flex justify-center items-center">
+                    <p class="font-JakartaSans font-bold text-sm">
+                      {{ data.title }}
+                    </p>
+                    <button v-if="data.jsonData" class="ml-2">
+                      <img :src="arrowicon" class="w-[9px] h-3" />
+                    </button>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+
+            <SkeletonLoadingTable :column="5" :row="5" />
+          </tableData>
+
           <div v-else>
             <tableData>
               <thead
@@ -599,7 +620,7 @@ tr th {
 
 .readmore-text {
   display: inline-block;
-  max-width: 200px; /* Atur sesuai kebutuhan */
+  max-width: 200px;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
