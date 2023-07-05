e3d917788f4f814e740a9870d8f27ef7ec1298ab
radenmasabdul <abdul.tamsis@gmail.com>
2023-07-05 16:17:51 +0700

    settlement report

--- src/views/travel-management/settlement/SettlementReport.vue
+++ src/views/travel-management/settlement/SettlementReport.vue
@@ -5,18 +5,20 @@ import Footer from "@/components/layout/Footer.vue";
 
 import tableContainer from "@/components/table/tableContainer.vue";
 import tableTop from "@/components/table/tableTop.vue";
+import tableData from "@/components/table/tableData.vue";
 
 import icon_receive from "@/assets/icon-receive.svg";
 import icon_filter from "@/assets/icon_filter.svg";
 import icon_reset from "@/assets/icon_reset.svg";
 import arrowicon from "@/assets/navbar/icon_arrow.svg";
+import Api from "@/utils/Api";
 
 import { ref, onBeforeMount, computed } from "vue";
 import { useSidebarStore } from "@/stores/sidebar.js";
 const sidebar = useSidebarStore();
 
-const selectedStatus = ref("Status");
-const selectedCatype = ref("Type");
+const selectedStatus = ref("");
+const selectedCatype = ref("");
 const date = ref();
 const search = ref("");
 let sortedData = ref([]);
@@ -24,14 +26,20 @@ let sortedbyASC = true;
 let instanceArray = [];
 let lengthCounter = 0;
 
+//for paginations & showing
 let showingValue = ref(1);
+let showingValueFrom = ref(0);
+let showingValueTo = ref(0);
 let pageMultiplier = ref(10);
 let pageMultiplierReactive = computed(() => pageMultiplier.value);
 let paginateIndex = ref(0);
+let totalPage = ref(0);
+let totalData = ref(0);
 
 const onChangePage = (pageOfItem) => {
   paginateIndex.value = pageOfItem - 1;
   showingValue.value = pageOfItem;
+  fetchSettlementReport(pageOfItem);
 };
 
 const selectAll = (checkValue) => {
@@ -51,19 +59,54 @@ const selectAll = (checkValue) => {
 
 const tableHead = [
   { Id: 1, title: "No", jsonData: "no" },
-  { Id: 2, title: "Created Date", jsonData: "created_date" },
-  { Id: 3, title: "Settlement No", jsonData: "settlement_no" },
-  { Id: 4, title: "Requestor", jsonData: "requestor" },
-  { Id: 5, title: "CA No", jsonData: "ca" },
-  { Id: 6, title: "Nominal Real", jsonData: "total" },
+  { Id: 2, title: "Created Date", jsonData: "created_at" },
+  { Id: 3, title: "Settlement No", jsonData: "no_settlement" },
+  { Id: 4, title: "Requestor", jsonData: "employee_name" },
+  { Id: 5, title: "CA No", jsonData: "no_ca" },
+  { Id: 6, title: "Nominal Real", jsonData: "total_real" },
   { Id: 7, title: "Status", jsonData: "status" },
 ];
+
+const sortList = (sortBy) => {
+  if (sortedbyASC) {
+    sortedData.value.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
+    sortedbyASC = false;
+  } else {
+    sortedData.value.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
+    sortedbyASC = true;
+  }
+};
 
 const getSessionForSidebar = () => {
   sidebar.setSidebarRefresh(sessionStorage.getItem("isOpen"));
 };
 
+const fetchSettlementReport = async (id) => {
+  const params = {
+    ca_type: null,
+    start_date: null,
+    end_date: null,
+    status: null,
+    search: search.value,
+    perPage: pageMultiplier.value,
+    page: id ? id : 1,
+  };
+  const token = JSON.parse(localStorage.getItem("token"));
+  Api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const res = await Api.get("/settlement/get_data", { params });
+  // console.log(res.data.data);
+  instanceArray = res.data.data;
+  sortedData.value = instanceArray.data;
+  console.log(instanceArray);
+  console.log(sortedData.value.length);
+  totalPage.value = instanceArray.last_page;
+  totalData.value = instanceArray.total;
+  showingValueFrom.value = instanceArray.from ? instanceArray.from : 0;
+  showingValueTo.value = instanceArray.to;
+};
+
 onBeforeMount(() => {
+  fetchSettlementReport();
   getSessionForSidebar();
 });
 </script>
@@ -113,13 +156,15 @@ onBeforeMount(() => {
                 type="text"
                 id="simple-search"
                 class="placeholder:text-slate-400 placeholder:font-JakartaSans placeholder:text-xs capitalize block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                placeholder="Search"
-                required
+                placeholder="Search..."
+                name="search"
+                v-model="search"
               />
             </div>
             <button
+              @click="fetchSettlementReport()"
               type="submit"
-              class="w-36 p-2.5 ml-2 text-sm rounded-lg font-medium text-white text-sm font-JakartaSans font-bold capitalize border-green bg-green gap-2 items-center hover:bg-[#099250] hover:text-white hover:border-[#099250]"
+              class="w-36 p-2.5 ml-2 text-sm rounded-lg font-medium text-white font-JakartaSans capitalize border-green bg-green gap-2 items-center hover:bg-[#099250] hover:text-white hover:border-[#099250]"
             >
               Search
             </button>
@@ -138,8 +183,9 @@ onBeforeMount(() => {
                   v-model="selectedCatype"
                 >
                   <option disabled selected>Type</option>
-                  <option>Travel</option>
-                  <option>Non Travel</option>
+                  <!-- <option v-for="data in sortedData" :value="data.id">
+                    {{ data.id_ca_type }}
+                  </option> -->
                 </select>
               </div>
 
@@ -168,8 +214,9 @@ onBeforeMount(() => {
                   v-model="selectedStatus"
                 >
                   <option disabled selected>Status</option>
-                  <option>Active</option>
-                  <option>Non Active</option>
+                  <!-- <option v-for="data in sortedData" :value="data.id">
+                    {{ data.status }}
+                  </option> -->
                 </select>
               </div>
 
@@ -218,72 +265,66 @@ onBeforeMount(() => {
           </div>
 
           <!-- TABLE -->
-          <div
-            class="px-4 py-2 bg-white rounded-b-xl box-border block overflow-x-hidden"
-          >
-            <div class="block overflow-x-auto">
-              <table
-                class="table table-zebra table-compact border w-full sm:w-full h-full rounded-lg"
-              >
-                <thead
-                  class="text-center font-JakartaSans text-sm font-bold h-10"
+          <tableData>
+            <thead class="text-center font-JakartaSans text-sm font-bold h-10">
+              <tr>
+                <th>
+                  <div class="flex justify-center">
+                    <input
+                      type="checkbox"
+                      name="checked"
+                      @click="selectAll((checkList = !checkList))"
+                    />
+                  </div>
+                </th>
+
+                <th
+                  v-for="data in tableHead"
+                  :key="data.Id"
+                  class="overflow-x-hidden cursor-pointer"
+                  @click="sortList(`${data.jsonData}`)"
                 >
-                  <tr>
-                    <th>
-                      <div class="flex justify-center">
-                        <input
-                          type="checkbox"
-                          name="checked"
-                          @click="selectAll((checkList = !checkList))"
-                        />
-                      </div>
-                    </th>
+                  <span class="flex justify-center items-center gap-1">
+                    {{ data.title }}
+                    <button>
+                      <img :src="arrowicon" class="w-[9px] h-3" />
+                    </button>
+                  </span>
+                </th>
+              </tr>
+            </thead>
 
-                    <th
-                      v-for="data in tableHead"
-                      :key="data.Id"
-                      class="overflow-x-hidden cursor-pointer"
-                      @click="sortList(`${data.jsonData}`)"
-                    >
-                      <span class="flex justify-center items-center gap-1">
-                        {{ data.title }}
-                        <button>
-                          <img :src="arrowicon" class="w-[9px] h-3" />
-                        </button>
-                      </span>
-                    </th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  <tr class="font-JakartaSans font-normal text-sm">
-                    <td>
-                      <input type="checkbox" name="checks" />
-                    </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+            <tbody>
+              <tr
+                class="font-JakartaSans font-normal text-sm"
+                v-for="data in sortedData"
+                :key="data.id"
+              >
+                <td>
+                  <input type="checkbox" name="checks" />
+                </td>
+                <td>{{ data.no }}</td>
+                <td>{{ data.created_at }}</td>
+                <td>{{ data.no_settlement }}</td>
+                <td>{{ data.employee_name }}</td>
+                <td>{{ data.no_ca }}</td>
+                <td>{{ data.total_real }}</td>
+                <td>{{ data.status }}</td>
+              </tr>
+            </tbody>
+          </tableData>
 
           <!-- PAGINATION -->
           <div
             class="flex flex-wrap justify-center lg:justify-between items-center mx-4 py-2"
           >
             <p class="font-JakartaSans text-xs font-normal text-[#888888] py-2">
-              Showing {{ (showingValue - 1) * pageMultiplier + 1 }} to
-              {{ Math.min(showingValue * pageMultiplier, sortedData.length) }}
-              of {{ sortedData.length }} entries
+              Showing {{ showingValueFrom }} to
+              {{ showingValueTo }}
+              of {{ totalData }} entries
             </p>
             <vue-awesome-paginate
-              :total-items="sortedData.length"
+              :total-items="totalData"
               :items-per-page="parseInt(pageMultiplierReactive)"
               :on-click="onChangePage"
               v-model="showingValue"
