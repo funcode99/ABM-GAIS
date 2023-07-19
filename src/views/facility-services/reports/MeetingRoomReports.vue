ad0c55bd09ce8d08d25a0f8ab517f4abdc18bdac
radenmasabdul <abdul.tamsis@gmail.com>
2023-07-19 11:08:31 +0700

    change color status

--- src/views/facility-services/reports/MeetingRoomReports.vue
+++ src/views/facility-services/reports/MeetingRoomReports.vue
@@ -360,7 +360,18 @@ const showClearButton = computed(() => {
                 <td>{{ data.employee_name }}</td>
                 <td>{{ data.duration }}</td>
                 <td>{{ data.name_meeting_room }}</td>
-                <td>{{ data.status }}</td>
+                <td>
+                  <span
+                    :class="
+                      data.status == 'Done'
+                        ? 'status-done'
+                        : data.status == 'Booked'
+                        ? 'status-default'
+                        : 'status-revision'
+                    "
+                    >{{ data.status }}</span
+                  >
+                </td>
               </tr>
             </tbody>
           </tableData>
@@ -497,5 +508,20 @@ input.nosubmit {
   background: transparent
     url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E")
     no-repeat 13px center;
+}
+
+.status-revision {
+  color: #ef3022;
+  font-weight: 800;
+}
+
+.status-default {
+  color: #2970ff;
+  font-weight: 800;
+}
+
+.status-done {
+  color: #00c851;
+  font-weight: 800;
 }
 </style>
