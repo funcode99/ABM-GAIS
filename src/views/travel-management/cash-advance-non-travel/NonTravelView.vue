94fde2bdf6d49e01966d1412807386be35df0c24
radenmasabdul <abdul.tamsis@gmail.com>
2023-08-21 16:40:21 +0700

    disabled cost center in detail table

--- src/views/travel-management/cash-advance-non-travel/NonTravelView.vue
+++ src/views/travel-management/cash-advance-non-travel/NonTravelView.vue
@@ -690,7 +690,8 @@ const inputClass =
                         v-else
                         v-model="item.id_cost_center"
                         required
-                        :disabled="item.id == idEdit ? false : true"
+                        disabled
+                        class="bg-white border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm cursor-pointer"
                       >
                         <option
                           v-for="data in listCostCentre"
